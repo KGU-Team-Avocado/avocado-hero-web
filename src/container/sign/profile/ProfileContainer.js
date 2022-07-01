b91777405c1b1e968adb339534a13b7d6f64456d
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-01 14:05:34 +0900

    프로필 페이지 추가

---
+++ src/container/sign/profile/ProfileContainer.js
@@ -0,0 +1,10 @@
+const ProfileContainer = () => {
+    return(
+        <div>
+            프로필이 올 곳
+            일단 계정 연동하지 말고 컴포넌트부터 대충 잡아놓고 시작하기
+        </div>
+    )
+}
+
+export default ProfileContainer;
