8ecc30067d7f6001f8d5428e2bca3c04daa63001
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-01 16:39:12 +0900

    axios, proxy 설치, CRUD 예제 페이지 추가중

--- src/container/example/ExampleContainer.js
+++ src/container/example/ExampleContainer.js
@@ -1,7 +1,19 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import Create from './crud/Create';
+import Delete from './crud/Delete';
+import Read from './crud/Read';
+import Update from './crud/Update';
+
 const ExampleContainer = () => {
+
     return(
         <div>
-            여기에다가 예제 적어주기
+            <h1>Node.js + Express.js + Mongoose + MongoDB 예제</h1>
+            <Create/>
+            <Read/>
+            <Update/>
+            <Delete/>
         </div>
     )
 }
---
+++ src/container/example/crud/Update.js
@@ -0,0 +1,5 @@
+export default () => {
+    return(
+        <div>Update</div>
+    )
+}
