9ce220b17278eca5694c23184d078ba7f6e7475c
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-01 14:01:26 +0900

    헤더 로그인 버튼 초안 작성중

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import { useState } from "react";
+
 
 const Header = () => {
+    const [isSignIn, setSignIn] = useState(false); //임시로 해놓음
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <Link to='/' className="navbar-brand" >Navbar</Link>
-                <a href="#"></a>
+                <Link to='/' className="navbar-brand" >Avocado Hero</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link to='/' className="nav-link active" aria-current="page" >Home</Link>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
@@ -24,7 +27,7 @@ const Header = () => {
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a className="dropdown-item" href="#">Action</a></li>
                                 <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
+                                <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
@@ -32,10 +35,25 @@ const Header = () => {
                             <a className="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    <div className="d-flex" >
+                        {
+                            // 로그인 했을 때를 true, 안했을 때를 false로 관리해주세요
+                            isSignIn
+                                ?
+                                <Dropdown>
+                                    <Dropdown.Toggle variant="" id="dropdown-basic" className="d-block link-dark text-decoration-none dropdown-toggle">
+                                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">프로필 보기</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2" onClick={()=>setSignIn(!isSignIn)}>로그아웃</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                :
+                                <button className="btn btn-outline-success" onClick={()=>setSignIn(!isSignIn)}>로그인</button>
+                        }
+                    </div>
                 </div>
             </div>
         </nav>
