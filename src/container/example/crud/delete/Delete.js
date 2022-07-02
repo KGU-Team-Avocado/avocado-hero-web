373691d972317f38a7a5ced48bf046ca0648e4b5
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-03 01:42:10 +0900

    삭제 예제 추가

--- src/container/example/crud/delete/Delete.js
+++ src/container/example/crud/delete/Delete.js
@@ -1,10 +1,14 @@
+import DeleteOne from "./DeleteOne"
+
 export default () => {
     return (
         <>
             <h2>
                 Delete
             </h2>
-            <div></div>
+            <div>
+                <DeleteOne/>
+            </div>
         </>
     )
 }
