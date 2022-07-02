f5eab313f5498e46d79cfc5d1223a39f2ae406f9
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-03 01:32:40 +0900

    랜덤 데이터 생성 기능 추가, ReadAll에서 새로고침 기능 추가

--- src/container/example/crud/create/CreateRandom.js
+++ src/container/example/crud/create/CreateRandom.js
@@ -1,11 +1,22 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 export default () => {
+    const [data, setData] = useState([]);
+
     const saveRandomData = () => {
-        console.log('axios가 나올 자리')
+        axios.get('/testsRouter/save').then((response) => {
+            console.log(JSON.stringify(response.data))
+            setData(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
     }
 
     return (
         <>
-            <button className='btn btn-outline-success' onClick={()=>saveRandomData()}>랜덤 데이터 생성하기 (제작중)</button>
+            <button className='btn btn-outline-success' onClick={() => saveRandomData()}>랜덤 데이터 생성하기</button>
+            <div>{JSON.stringify(data)}</div>
         </>
     )
 }
--- src/container/example/crud/read/ReadAll.js
+++ src/container/example/crud/read/ReadAll.js
@@ -4,21 +4,28 @@ import axios from 'axios';
 export default () => {
     const [data, setData] = useState([]);
     useEffect(() => {
-        (async () => {
-            // 데이터 받아오는 작업 시작
-            axios.get('/testsRouter/find')
-                .then((response) => {
-                    console.log(JSON.stringify(response.data))
-                    setData(response.data);
-                }).catch(function (error) {
-                    console.log(error);
-                });
-            // 데이터 받아오는 작업 끝
-        })();
+        findAll();
     }, []);
+
+    const findAll = () => {
+        // 데이터 받아오는 작업 시작
+        axios.get('/testsRouter/find')
+            .then((response) => {
+                console.log(JSON.stringify(response.data))
+                setData(response.data);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        // 데이터 받아오는 작업 끝
+    }
+
     return (
         <>
-            <h4>전체 데이터 가져오기</h4>
+            <div className='d-flex justify-content-between'>
+                <h4>전체 데이터 가져오기</h4>
+                <button className='btn btn-outline-success' onClick={() => findAll()}>새로고침</button>
+
+            </div>
             <div>
                 {
                     data.length == 0
