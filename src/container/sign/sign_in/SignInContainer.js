5026a126ddff51a9052e6475ffda70781495659f
seeun01 <ssky601@gmail.com>
2022-07-07 14:49:19 +0900

    로그인 기능 구현중

--- src/container/sign/sign_in/SignInContainer.js
+++ src/container/sign/sign_in/SignInContainer.js
@@ -1,7 +1,82 @@
+import axios from "axios";
+import { useState } from "react";
+
 const SignInContainer = () => {
-    return(
-        <>로그인 관련 메뉴</>
-    )
-}
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleId = (e) => {
+    setId(e.currentTarget.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const login = () => {
+    console.log(id, password);
+    axios
+      .post("usersRouter/login", {
+        id: id,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      {/* <img
+        className="mb-4"
+        src="/docs/5.2/assets/brand/bootstrap-logo.svg"
+        alt=""
+        width="72"
+        height="57"
+      /> */}
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div className="form-floating">
+        <input
+          type="id"
+          className="form-control"
+          id="floatingInput"
+          placeholder="아이디"
+          onChange={handleId}
+          value={id}
+        />
+        <label htmlFor="floatingInput">ID</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          onChange={handlePassword}
+          value={password}
+        />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <button
+        className="w-100 btn btn-lg btn-primary"
+        type="submit"
+        onClick={() => login()}
+      >
+        Sign in
+      </button>
+      <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+    </div>
+  );
+};
 
 export default SignInContainer;
