e8ac9f58f7f688afe29f8e0fc488b89dea79e5cf
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-07-15 16:32:59 +0900

    그룹 리스트 뜨는거 부터 구현중

--- src/component/group/card/GroupCard.js
+++ src/component/group/card/GroupCard.js
@@ -1,7 +1,30 @@
 export default (props) => {
     return (
-        <div className="col-md-4 my-2">
-            <div className="card">{props.title}</div>
+        <div className="col-xl-4 my-2">
+            <a className="text-decoration-none text-dark" href="#">
+                <div className="card p-3">
+                    <div className="row">
+                        <div className="text-center col-md-4 pe-2 py-5">
+                            <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        </div>
+                        <div className="col-md-8 ps-5 py-3">
+                            <h3>{props.group.name}</h3>
+                            <h4>{props.group.title}</h4>
+                            <p>{props.group.description}</p>
+                            <div>
+                                <span className="badge text-bg-primary">Primary</span>
+                                <span className="badge text-bg-secondary">Secondary</span>
+                                <span className="badge text-bg-success">Success</span>
+                                <span className="badge text-bg-danger">Danger</span>
+                                <span className="badge text-bg-warning">Warning</span>
+                                <span className="badge text-bg-info">Info</span>
+                                <span className="badge text-bg-light">Light</span>
+                                <span className="badge text-bg-dark">Dark</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
     )
 }
--- src/container/group/finder/GroupFinderContainer.js
+++ src/container/group/finder/GroupFinderContainer.js
@@ -3,6 +3,32 @@ import GroupCard from "../../../component/group/card/GroupCard"
 
 export default () => {
 
+    const [groups, setGroups] = useState([
+        {
+            _id: 0,
+            name:"아보카도",
+            title: "콘솔",
+            description: "우리 같이 개발해요",
+        },
+        {
+            _id: 1,
+            name:"아보카도",
+            title: "리액트 튜토리얼",
+            description: "우리 같이 개발해요",
+        },
+        {
+            _id: 2,
+            name:"아보카도",
+            title: "히어로",
+            description: "우리 같이 개발해요",
+        },
+        {
+            _id: 3,
+            name:"팀 명",
+            title: "프로젝트명",
+            description: "우리 같이 개발해요 우리 같이 개발해요 ",
+        },
+    ]);
 
     return (
         <>
@@ -11,7 +37,18 @@ export default () => {
                 <p>훌륭한 팀을 구해보아요</p>
             </div>
             <div className="row">
-
+                {
+                    groups.length > 0
+                        ?
+                        groups.map((group) => (
+                            <GroupCard
+                                key={group._id}
+                                group={group}
+                            />
+                        ))
+                        :
+                        <div>그룹이 없습니다.</div>
+                }
             </div>
         </>
     )
