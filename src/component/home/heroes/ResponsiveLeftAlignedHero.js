aa870c0cd3791d8893d4ff157e401789a30fe820
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-16 15:49:05 +0900

    hero 반응형 오류 수정

--- src/component/home/heroes/ResponsiveLeftAlignedHero.js
+++ src/component/home/heroes/ResponsiveLeftAlignedHero.js
@@ -1,6 +1,6 @@
 export default () => {
     return (
-        <div class="container col-xxl-8 px-4 py-5">
+        <div class="container col-xxl-10 px-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-10 col-sm-8 col-lg-6">
                     <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
