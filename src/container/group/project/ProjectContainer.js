2412f4f87031d8f2516497363f51c3c11416fd25
soyoung125 <sypark041754@gmail.com>
2022-07-25 00:23:16 +0900

    공지 사항 추가 구현

--- src/container/group/project/ProjectContainer.js
+++ src/container/group/project/ProjectContainer.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default () => {
-    const [notices, setNotices] = useState([
+    const [notices, setNotices] = useState([ //공지사항 배열
         {
             _id: 0,
             title: "콘솔",
@@ -24,6 +24,18 @@ export default () => {
         },
     ]);
 
+    const [title, setTitle] = useState(""); //공지사항 제목
+    const [description, setDescription] = useState(""); //공지사항 내용
+    const [id, setId] = useState(4); //추후 0으로 교체
+
+    const saveNewNotice = () => {
+        const newNotice = {_id: id, title: title, description: description};
+        setNotices([newNotice, ...notices]);
+        setId(id + 1);
+        setTitle("");
+        setDescription("");
+    }
+
     return (
         <>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -33,7 +45,7 @@ export default () => {
                         <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
                         <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
                     </div>
-                    <button type="button" className="btn btn-sm btn-outline-secondary">
+                    <button type="button" className="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
                         공지사항 수정하기
                     </button>
                 </div>
@@ -41,19 +53,46 @@ export default () => {
 
             <div className="accordion mb-3" id="accordionPanelsStayOpenExample">
                 {notices.map((notice) => (
-                    <div className="accordion-item" key={notice._id} id={"panelsStayOpen-heading"+notice._id}>
+                    <div className="accordion-item" key={notice._id} id={"panelsStayOpen-heading" + notice._id}>
                         <h2 className="accordion-header">
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={"#panelsStayOpen-collapse"+notice._id} aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={"#panelsStayOpen-collapse" + notice._id} aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                                 {notice.title}
                             </button>
                         </h2>
-                        <div id={"panelsStayOpen-collapse"+notice._id} className="accordion-collapse collapse show" aria-labelledby={"panelsStayOpen-heading"+notice._id}>
+                        <div id={"panelsStayOpen-collapse" + notice._id} className="accordion-collapse collapse show" aria-labelledby={"panelsStayOpen-heading" + notice._id}>
                             <div className="accordion-body">
                                 {notice.description}
                             </div>
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">새 공지사항</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="recipient-name" class="col-form-label">제목</label>
+                                    <input type="text" class="form-control" id="recipient-name" value={title} onChange={(e) => setTitle(e.target.value)} />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="message-text" class="col-form-label">내용</label>
+                                    <textarea class="form-control" id="message-text" value={description} onChange={(e) => setDescription(e.target.value)} ></textarea>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+                            <button type="button" class="btn btn-primary" onClick={() => saveNewNotice()} data-bs-dismiss="modal">저장</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
