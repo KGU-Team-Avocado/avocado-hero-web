68e2588a58fcc7a056c55206d25348c99efb2717
hido02 <bestdohee@gmail.com>
2022-07-28 15:27:21 +0900

    프로필 수정 중

--- src/container/sign/profile/ProfileContainer.js
+++ src/container/sign/profile/ProfileContainer.js
@@ -6,6 +6,9 @@ import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
+import { MultiSelect } from "react-multi-select-component";
+
+
 
 const ProfileContainer = () => {
 
@@ -40,6 +43,7 @@ const ProfileContainer = () => {
         email: '',
         phoneNum: '',
         birth: '',
+        belong: '',
       });
 
       const onChange =(e) => { // 입력 함수
@@ -50,7 +54,7 @@ const ProfileContainer = () => {
         });
       };
 
-      const { nickname, email, phoneNum, birth } = inputs;
+      const { nickname, email, phoneNum, birth, belong } = inputs;
 
     const onClickSubmit = (e) => { 
         e.preventDefault();
@@ -59,6 +63,10 @@ const ProfileContainer = () => {
             .post("/usersRouter/profileUpdate", {
                 user_id: user_id,
                 user_nickname: nickname,
+                user_email: email,
+                user_phoneNum: phoneNum,
+                user_birth: birth,
+                user_belong: belong,
             })
             .then((response) => {
                 console.log(response);
@@ -73,16 +81,23 @@ const ProfileContainer = () => {
             setIsEdit(!isEdit);
     };
 
-    // const [newNickname, setNewNickname] = useState(profile.name);
+    const options = [
+        { label: "React", value: "react" },
+        { label: "Node.js", value: "nodejs" },
+        { label: "Mongo DB", value: "mongodb" },
+        { label: "??", value: "??", disabled: true },
+    
+    ];
 
-    // const nicknameInput = (e) => {
-    //     setNewNickname(e.target.value);
-    // };
+    // const [countList, setCountList] = useState([0])
 
-    // const onClickSubmit = () => {
-    //     const profile = profile.map((item) => {
-    //         ...item,
-    //     })
+    // const onAddDetailDiv = () => {
+    //   let countArr = [...countList]
+    //   let counter = countArr.slice(-1)[0]
+    //   counter += 1
+    //   countArr.push(counter)	// index 사용 X
+    //   // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+    //   setCountList(countArr)
     // }
 
     return (
@@ -101,11 +116,39 @@ const ProfileContainer = () => {
                                     src={Image} style={{ height: '150px' }} /></div>
                                     <div><Button>변경</Button></div>
                                     </div>
-                                    테스트로 닉네임만 수정 중인데 안 됨
                                 <Form.Control type="text" name="nickname" value={nickname} placeholder="닉네임" onChange={onChange} />
+                                <Form.Control type="text" name="email" value={email} placeholder="이메일" onChange={onChange} />
+                                <Form.Control type="text" name="phoneNum" value={phoneNum} placeholder="전화번호" onChange={onChange} />
+                                <Form.Control type="text" name="birth" value={birth} placeholder="생년월일" onChange={onChange} /> 
+                                {/* date형으로 바꿔야 함 */}
                             </Card.Body>
                             </Card>
                             </div>
+                            <div class="col-md-9">
+                            <Card style={{ height: '30rem', margin: '10px 0' }}>
+                            <Card.Body>
+                                <Card.Title>기본 정보</Card.Title>
+                                <Card.Text>
+                                <MultiSelect
+                                options={options}
+                                // value={selected}
+                                // onChange={setSelected}
+                                labelledBy="Select"
+                            />    
+
+
+      {/* <DetailList countList={countList} /> */}
+      {/* <Button onClick={onAddDetailDiv}>
+        추가
+      </Button> */}
+                                    
+                                    {/* <Form.Control type="text" name="belong" value={belong} placeholder="소속" onChange={onChange} /> 
+                                    <Button onClick={onAddText}>+</Button> */}
+                                
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </div>
                             <Button onClick={onClickSubmit}>프로필 수정</Button>
                                 </Row>
                 </Container>
