0b2096ee46968d60e9e9bb823e10cfd3f30bcabb
soyoung125 <sypark041754@gmail.com>
2022-07-28 23:20:04 +0900

    캘린더 수정하는 중

--- src/container/group/project/ProjectCalendarContainer.js
+++ src/container/group/project/ProjectCalendarContainer.js
@@ -5,24 +5,30 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 export default () => {
     const [events, setEvents] = useState([{
-        id: 0,
+        id: 0,  // 매주 반복하는 일정
         title: "All Day Event very long title",
-        allDay: true,
-        start: new Date(2022, 6, 0),
-        end: new Date(2022, 6, 1)
+        daysOfWeek: [ '4' ],
+        startTime: '10:45:00',
+        endTime: '12:45:00',
+        color: 'red'
     },
     {
-        id: 1,
+        id: 1, // 하루 일정 (종일)
         title: "Long Event",
-        start: new Date(2022, 6, 7),
-        end: new Date(2022, 6, 10)
+        date: '2022-07-11'
     },
-
     {
-        id: 2,
+        id: 2, // 하루 일정 (특정 시간)
+        title: "Long Event",
+        start: '2022-07-11T10:45:00',
+        end: '2022-07-11T11:45:00',
+    },
+    {
+        id: 3, // 긴 일정
+        groupId: 'blueEvents',
         title: "DTS STARTS",
-        start: new Date(2022, 6, 13, 0, 0, 0),
-        end: new Date(2022, 6, 20, 0, 0, 0)
+        start: '2022-07-10T10:45:00',
+        end: '2022-07-13T10:45:00'
     }]);
 
     const handleDateClick = (arg) => { // bind with an arrow function
@@ -30,21 +36,7 @@ export default () => {
     }
 
     return (
-        <>
-            {/* <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 className="h2">일정</h1>
-                <div className="btn-toolbar mb-2 mb-md-0">
-                    <div className="btn-group me-2">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
-                    </div>
-                    <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-calendar align-text-bottom" aria-hidden="true"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                        This week
-                    </button>
-                </div>
-
-            </div> */}
+        <div className='mt-3'>
             <div className='calendar-container'>
                 <FullCalendar
                     plugins={[dayGridPlugin, interactionPlugin]}
@@ -54,11 +46,18 @@ export default () => {
                     selectable={true}
                     dateClick={handleDateClick} // 날짜 하나 선택
                     select={handleDateClick} //여러 날짜 드래그로 선택
-                    editable={true} // 일정 드래그앤드롭으로 변경
+                    editable={true} // 일정 드래그앤드롭으로 변경 (allday가 아닌 일정은 늘리고 줄이기 불가)
                     droppable={true}
+                    dayMaxEventRows={true}  // 하루에 보여지는 일정 수 조정(나머지는 +n 으로 표시)
+                    headerToolbar= {{
+                        left: 'prevYear,prev',
+                        center: 'title',
+                        right: 'next,nextYear'
+                      }}
                     eventResizableFromStart={true}
+                    themeSystem={'bootstrap5'}
                 />
             </div>
-        </>
+        </div>
     )
 }
