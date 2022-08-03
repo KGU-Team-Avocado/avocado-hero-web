759571877bcfe5d3a172b9385f21afef00252140
soyoung125 <sypark041754@gmail.com>
2022-08-04 00:09:40 +0900

    멤버관리 구현

--- src/container/group/project/ProjectMembersContainer.js
+++ src/container/group/project/ProjectMembersContainer.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
 
 export default () => {
     const [applicants, setApplicants] = useState([ // 프로젝트 지원자 배열
@@ -31,20 +32,30 @@ export default () => {
             joined: false
         },
     ]);
+    const [show, setShow] = useState('null');
+    const [close, setClose] = useState(false)
+
+    const handleClose = () => setShow('null');
+    const handleShow = (applicant) => {
+        console.log(show);
+        setShow(applicant);
+    }
 
     const acceptMember = (applicant) => {
-        setApplicants(applicants.map((user) => user.user_id === applicant.user_id ? {...user, joined: true} : user))
+        setShow('null');
+        setApplicants(applicants.map((user) => user.user_id === applicant.user_id ? { ...user, joined: true } : user));
     }
 
     const rejectMember = (applicant) => {
-        if(!window.confirm('변려한 신청자는 신청자 목록에서 사라지게 됩니다. 삭제하시겠습니까?')){
+        setShow('null');
+        if (!window.confirm('변려한 신청자는 신청자 목록에서 사라지게 됩니다. 삭제하시겠습니까?')) {
             return;
         }
         setApplicants(applicants.filter((user) => user.user_id !== applicant.user_id))
     }
 
     const cancleAccept = (applicant) => {
-        setApplicants(applicants.map((user) => user.user_id === applicant.user_id ? {...user, joined: false} : user))
+        setApplicants(applicants.map((user) => user.user_id === applicant.user_id ? { ...user, joined: false } : user))
     }
 
     return (
@@ -63,71 +74,87 @@ export default () => {
                 </div>
             </div>
 
-            <div class="d-flex justify-content-between">
-                <h2>신청자 목록</h2>
-                <button type="button" className="btn btn-success btn-sm" onClick={() => console.log('finish')} >마감</button>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead class="table-light text-center">
+            {close ? null :
+                <>
+                    <div className="d-flex justify-content-between">
+                        <h2>신청자 목록</h2>
+                        <button type="button" className="btn btn-success btn-sm" onClick={() => setClose(true)} >마감</button>
+                    </div>
+                    <div className="table-responsive">
+                        <table className="table table-hover">
+                            <thead className="table-light text-center">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">아이디</th>
+                                    <th scope="col">이름</th>
+                                    <th scope="col">이메일</th>
+                                    <th scope="col">승인/반려</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table-group-divider text-center">
+                                {applicants.map((applicant) => (
+                                    applicant.joined ? null :
+                                        <tr key={applicant.user_id}>
+                                            <th onClick={() => handleShow(applicant)} scope="row">1</th>
+                                            <td onClick={() => handleShow(applicant)}>{applicant.user_id}</td>
+                                            <td onClick={() => handleShow(applicant)}>{applicant.name}</td>
+                                            <td onClick={() => handleShow(applicant)}>{applicant.email}</td>
+                                            <td>
+                                                <button type="button" className="btn btn-primary btn-sm me-2" onClick={() => acceptMember(applicant)} >승인</button>
+                                                <button type="button" className="btn btn-danger btn-sm" onClick={() => rejectMember(applicant)} >반려</button>
+                                            </td>
+                                        </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+            }
+
+
+            <div className="table-responsive">
+                <h2>현재 팀원</h2>
+                <table className="table table-hover">
+                    <thead className="table-light text-center">
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">아이디</th>
                             <th scope="col">이름</th>
                             <th scope="col">이메일</th>
-                            <th scope="col">승인/반려</th>
+                            {close ? null :
+                                <th scope="col">승인 취소</th>
+                            }
                         </tr>
                     </thead>
-                    <tbody class="table-group-divider text-center">
+                    <tbody className="table-group-divider text-center">
                         {applicants.map((applicant) => (
-                            applicant.joined ? null :
-                                <tr>
+                            !applicant.joined ? null :
+                                <tr key={applicant.user_id}>
                                     <th scope="row">1</th>
                                     <td>{applicant.user_id}</td>
                                     <td>{applicant.name}</td>
                                     <td>{applicant.email}</td>
-                                    <td>
-                                        {/* <div className="d-inline-flex"> */}
-                                        <button type="button" className="btn btn-primary btn-sm me-2" onClick={() => acceptMember(applicant)} >승인</button>
-                                        <button type="button" className="btn btn-danger btn-sm" onClick={() => rejectMember(applicant)} >반려</button>
-                                        {/* </div> */}
-                                    </td>
+                                    {close ? null :
+                                        <td>
+                                            <button type="button" className="btn btn-danger btn-sm" onClick={() => cancleAccept(applicant)} >취소</button>
+                                        </td>
+                                    }
                                 </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
 
-            <div class="table-responsive">
-                <h2>현재 팀원</h2>
-                <table class="table table-hover">
-                    <thead class="table-light text-center">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">아이디</th>
-                            <th scope="col">이름</th>
-                            <th scope="col">이메일</th>
-                            <th scope="col">승인 취소</th>
-                        </tr>
-                    </thead>
-                    <tbody class="table-group-divider text-center">
-                        {applicants.map((applicant) => (
-                            !applicant.joined ? null :
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>{applicant.user_id}</td>
-                                    <td>{applicant.name}</td>
-                                    <td>{applicant.email}</td>
-                                    <td>
-                                        {/* <div className="d-inline-flex"> */}
-                                        <button type="button" className="btn btn-danger btn-sm" onClick={() => cancleAccept(applicant)} >취소</button>
-                                        {/* </div> */}
-                                    </td>
-                                </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <Modal show={show !== 'null'} onHide={handleClose} animation={false} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{show.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{show.about_me}</Modal.Body>
+                <Modal.Footer>
+                    <button type="button" className="btn btn-primary btn-sm me-2" onClick={() => { acceptMember(show) }} >승인</button>
+                    <button type="button" className="btn btn-danger btn-sm" onClick={() => rejectMember(show)} >반려</button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
