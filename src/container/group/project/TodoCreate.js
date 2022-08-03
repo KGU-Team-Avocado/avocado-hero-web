5f56a33f2bdff49abb86994997308ece48e4b6db
seeun01 <ssky601@gmail.com>
2022-08-04 00:04:32 +0900

    Todo 할 일 추가 기능 수정

--- src/container/group/project/TodoCreate.js
+++ src/container/group/project/TodoCreate.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../project/Todo.css";
 import Form from "react-bootstrap/Form";
+import ReactDOM from "react-dom";
 
 const TodoCreate = ({ onCreate }) => {
+  const todoInput = useRef();
+
   const onCheckEnter = (e) => {
     if (e.key === "Enter") {
+      if (e.target.value.length < 1) {
+        todoInput.current.focus();
+        return;
+      }
       console.log(e.target.value);
       onCreate(e.target.value);
+      todoInput.current.value = "";
     }
   };
 
   return (
     <div className="TodoCreate">
       <Form.Control
+        ref={todoInput}
         type="text"
         placeholder="할 일 추가"
         onKeyPress={onCheckEnter}
