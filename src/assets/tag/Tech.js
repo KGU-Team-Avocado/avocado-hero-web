2dc58d5f36f0f01b12bb44e47bce806d1ba823e1
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-08-05 21:47:14 +0900

    테크 색상 수정

--- src/assets/tag/Tech.js
+++ src/assets/tag/Tech.js
@@ -2,9 +2,9 @@ export const options = [
     { label: "React", value: "react", type:"fe", bgColor:'#003399', txtColor:'#FFFFFF'},
     { label: "Node.js", value: "nodejs", type:"be", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
     { label: "Mongo DB", value: "mongodb", type:"db", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
-    { label: "Express.js", value: "expressjs", type:"be", bgColor:'#FFFFFF', txtColor:'#000000'},
-    { label: "Java", value: "java", type:"be", bgColor:'#FFFFFF', txtColor:'#FF0000'},
-    { label: "JavaScript", value: "javascript", type:"fe", bgColor:'#FFFFFF', txtColor:'#FFBB00'},
+    { label: "Express.js", value: "expressjs", type:"be", bgColor:'#F6F6F6', txtColor:'#000000'},
+    { label: "Java", value: "java", type:"be", bgColor:'#FF0000', txtColor:'#FFFFFF'},
+    { label: "JavaScript", value: "javascript", type:"fe", bgColor:'#FFBB00', txtColor:'#FFFFFF'},
     { label: "??", value: "??", bgColor:"", txtColor:'#FFFFFF', disabled: true },
 ];
 
