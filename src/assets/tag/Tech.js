da3811fbc0258821d267b4dcd002bf16cf030a36
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-08 15:09:05 +0900

    기술 종류 추가

--- src/assets/tag/Tech.js
+++ src/assets/tag/Tech.js
@@ -1,11 +1,26 @@
 export const options = [
-    { label: "React", value: "react", type:"fe", bgColor:'#003399', txtColor:'#FFFFFF'},
-    { label: "Node.js", value: "nodejs", type:"be", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
-    { label: "Mongo DB", value: "mongodb", type:"db", bgColor:'#2F9D27', txtColor:'#FFFFFF'},
-    { label: "Express.js", value: "expressjs", type:"be", bgColor:'#F6F6F6', txtColor:'#000000'},
-    { label: "Java", value: "java", type:"be", bgColor:'#FF0000', txtColor:'#FFFFFF'},
-    { label: "JavaScript", value: "javascript", type:"fe", bgColor:'#FFBB00', txtColor:'#FFFFFF'},
-    { label: "??", value: "??", bgColor:"", txtColor:'#FFFFFF', disabled: true },
+    { type: "fe", label: "React", value: "react", bgColor: "#004080", txtColor: '#FFFFFF' },
+    { type: "fe", label: "React Natvie", value: "react-native", bgColor: '#004080', txtColor: '#FFFFFF' },
+    { type: "fe", label: "Native Base", value: "native-base", bgColor: "#0080c0", txtColor: '#FFFFFF' },
+    { type: "fe", label: "Bootstrap", value: "bootstrap", bgColor: "#8000ff", txtColor: '#FFFFFF' },
+    { type: "be", label: "Node.js", value: "node-js", bgColor: '#2F9D27', txtColor: '#FFFFFF' },
+    { type: "db", label: "Mongo DB", value: "mongo-db", bgColor: '#2F9D27', txtColor: '#FFFFFF' },
+    { type: "db", label: "MySQL", value: "mysql", bgColor: "#0080c0", txtColor: '#FFFFFF' },
+    { type: "db", label: "MariaDB", value: "mariadb", bgColor: "#0080c0", txtColor: '#FFFFFF' },
+    { type: "be", label: "Express.js", value: "express-js", bgColor: '#F6F6F6', txtColor: '#000000' },
+    { type: "be", label: "Java", value: "java", bgColor: '#FF0000', txtColor: '#FFFFFF' },
+    { type: "fe", label: "Python", value: "python", bgColor: "#0080c0", txtColor: '#FFFFFF' },
+    { type: "be", label: "JSP", value: "jsp", bgColor: '#FF0000', txtColor: '#FFFFFF' },
+    { type: "fe", label: "JavaScript", value: "javascript", bgColor: '#FFBB00', txtColor: '#FFFFFF' },
+    { type: "fe", label: "jQuery", value: "jquery", bgColor: '#FFBB00', txtColor: '#FFFFFF' },
+    { type: "fe", label: "CSS", value: "css", bgColor: "#0080ff", txtColor: '#FFFFFF' },
+    { type: "fe", label: "HTML", value: "html", bgColor: "#ff8000", txtColor: '#FFFFFF' },
+    { type: "fe", label: "Expo", value: "expo", bgColor: "#000000", txtColor: '#FFFFFF' },
+    { type: "etc", label: "Tomcat", value: "tomcat", bgColor: "#000000", txtColor: '#FFFFFF' },
+    { type: "etc", label: "Ubuntu", value: "ubuntu", bgColor: "#000000", txtColor: '#FFFFFF' },
+    { label: "??", value: "??", bgColor: "", txtColor: '#FFFFFF', disabled: true },
+    { label: "??", value: "??", bgColor: "", txtColor: '#FFFFFF', disabled: true },
 ];
 
-// 속성 값 수정 절대 금지
+
+// 속성 이름 수정 절대 금지 + 항목 추가는 괜찮음
