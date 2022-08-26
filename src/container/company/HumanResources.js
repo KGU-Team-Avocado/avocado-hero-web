61a5cee1ffe56c7cf45d841354df7e7ffb533060
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-08-26 17:56:29 +0900

    인재찾기 기능 추가중

--- src/container/company/HumanResources.js
+++ src/container/company/HumanResources.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import * as API from "../../api/API"
+
 const HumanResources = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+      getUsers();
+  },[]);
+
+  const getUsers = async () => {
+      //현재 유저 스키마가 일정하지 않아서 생기는 오류가 있음... 수정 예정
+      setUsers(await API.findUsers());
+  }
+
+
+
   //인재찾기 페이지
   return (
     <>
-      <h2>HumanResources</h2>
+      <h2>인재 찾기</h2>
+      {JSON.stringify(users)}
+
     </>
   );
 };
