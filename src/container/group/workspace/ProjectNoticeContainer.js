62f8a9991f4a3d42a9110c16940ddc2d0127b3d9
soyoung125 <sypark041754@gmail.com>
2022-08-28 22:46:57 +0900

    우측 상단 불필요한 버튼 제거

--- src/container/group/workspace/ProjectNoticeContainer.js
+++ src/container/group/workspace/ProjectNoticeContainer.js
@@ -102,10 +102,6 @@ const ProjectContainer = () => {
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 className="h2">공지사항</h1>
                 <div className="btn-toolbar mb-2 mb-md-0">
-                    <div className="btn-group me-2">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
-                    </div>
                     {
                         user.user_id === groupManager ?
                             <button type="button" className="btn btn-sm btn-outline-secondary" onClick={() => setShow(true)} >
