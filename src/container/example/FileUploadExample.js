3a2e53e895824fa98fb0c535a4afd997746d8347
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-09-03 15:32:01 +0900

    테스트 페이지에 파일 업로드 기능 구현 성공 (DB연동은 아직 안됨)

--- src/container/example/FileUploadExample.js
+++ src/container/example/FileUploadExample.js
@@ -1,40 +1,82 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import { useState } from 'react'
 
 function FileUploadExample() {
+  const [image, setImage] = useState({ preview: '', data: '' })
+  const [status, setStatus] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let formData = new FormData()
+    formData.append('file', image.data)
+    const response = await fetch('/testsRouter/uploadFile', {
+      method: 'POST',
+      body: formData,
+    })
 
-  const [file, setFile] = useState()
-
-  function handleChange(event) {
-    setFile(event.target.files[0])
+    if (response) setStatus(response.statusText)
   }
-  
-  function handleSubmit(event) {
-    event.preventDefault()
-    const url = '/testsRouter/uploadFile';
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-      },
-    };
-    axios.post(url, formData, config).then((response) => {
-      console.log(response.data);
-    });
 
+  const handleFileChange = (e) => {
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    }
+    setImage(img)
   }
 
   return (
-    <div className="App">
-        <form onSubmit={handleSubmit}>
-          <h1>React File Upload</h1>
-          <input type="file" onChange={handleChange}/>
-          <button type="submit">Upload</button>
-        </form>
+    <div className='App'>
+      <h1>Upload to server</h1>
+      {image.preview && <img src={image.preview} width='100' height='100' />}
+      <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <input type='file' name='file' onChange={handleFileChange}></input>
+        <button type='submit'>Submit</button>
+      </form>
+      {status && <h4>{status}</h4>}
     </div>
-  );
+  )
 }
 
-export default FileUploadExample;
+export default FileUploadExample
+
+
+// import React, {useState} from 'react';
+// import axios from 'axios';
+
+// function FileUploadExample() {
+
+//   const [file, setFile] = useState()
+
+//   function handleChange(event) {
+//     setFile(event.target.files[0])
+//   }
+  
+//   function handleSubmit(event) {
+//     event.preventDefault()
+//     const url = '/testsRouter/uploadFile';
+//     const formData = new FormData();
+//     formData.append('file', file);
+//     formData.append('fileName', file.name);
+//     const config = {
+//       headers: {
+//         'content-type': 'multipart/form-data',
+//       },
+//     };
+//     axios.post(url, formData, config).then((response) => {
+//       console.log(response.data);
+//     });
+
+//   }
+
+//   return (
+//     <div className="App">
+//         <form onSubmit={handleSubmit}>
+//           <h1>React File Upload</h1>
+//           <input type="file" onChange={handleChange}/>
+//           <button type="submit">Upload</button>
+//         </form>
+//     </div>
+//   );
+// }
+
+// export default FileUploadExample;
