2847cc6dca855a52ebb06eaa5aafa4d370d25242
seeun01 <ssky601@gmail.com>
2022-09-15 15:47:55 +0900

    투두리스트 체크표시, 삭제기능 디비연동

--- src/container/group/workspace/TodoItem.js
+++ src/container/group/workspace/TodoItem.js
@@ -1,29 +1,42 @@
+import axios from "axios";
 import React, { useCallback, useState } from "react";
 import { BsCheckCircleFill, BsCheckCircle } from "react-icons/bs";
 import "../workspace/Todo.css";
 
 const TodoItem = ({ todo, onDelete }) => {
-  const { id, todo_text, checked } = todo;
+  const { _id, project_id, todo_text, todo_isChecked } = todo;
 
   //handleCheck에서 todo의 checked를 변경 후 재렌더링해주기 위해 사용
   const [, updateState] = useState();
   const forceUpdate = useCallback(() => updateState({}, []));
 
   const handleCheck = () => {
-    if (todo.checked) {
-      todo.checked = false;
+    if (todo.todo_isChecked) {
+      todo.todo_isChecked = false;
+      //isChecked 수정
+      axios
+      .post("/todosRouter/updateIsChecked", {
+        _id : todo._id,
+        todo_isChecked : todo.todo_isChecked,
+      })
     } else {
-      todo.checked = true;
+      todo.todo_isChecked = true;
+      //isChecked 수정
+      axios
+      .post("/todosRouter/updateIsChecked", {
+        _id : todo._id,
+        todo_isChecked : todo.todo_isChecked,
+      })
     }
-    console.log(todo.checked);
+    console.log(todo.todo_isChecked);
     forceUpdate();
   };
 
   return (
     <div className="TodoItem">
       <div onClick={handleCheck}>
-        <div className={`content ${checked ? "checked" : " "}`}>
-          {checked ? <BsCheckCircleFill /> : <BsCheckCircle />}
+        <div className={`content ${todo_isChecked ? "checked" : " "}`}>
+          {todo_isChecked ? <BsCheckCircleFill /> : <BsCheckCircle />}
           <div className="text">{todo_text}</div>
         </div>
       </div>
@@ -35,7 +48,7 @@ const TodoItem = ({ todo, onDelete }) => {
           type="button"
           class="btn btn-secondary me-2"
           onClick={() => {
-            onDelete(todo.id);
+            onDelete(todo._id);
           }}
         >
           삭제
