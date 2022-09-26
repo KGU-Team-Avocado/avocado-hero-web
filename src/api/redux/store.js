b1d094417b23ff1e588c49a48adb0963fb493abf
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-09-26 16:44:32 +0900

    redux-persist 적용으로 새로고침 시 안날라감

--- src/api/redux/store.js
+++ src/api/redux/store.js
@@ -1,10 +1,30 @@
+
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slice/counterSlice.js';
 import userReducer from './user/userSlice.js';
 
+// import storage from 'redux-persist/lib/storage'; // for local storage
+import storage from 'redux-persist/lib/storage/session' // for session storage
+
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  counter: counterReducer,
+  user: userReducer,
+  // 리듀서 추가를 여기에 해주삼 ㅇㅇ
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
