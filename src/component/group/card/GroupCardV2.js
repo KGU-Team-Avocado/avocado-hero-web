19599fe3496d7794a921af6a86d9d07d96c9466e
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-02 18:19:13 +0900

    카드 디자인 새로운걸로 반영

--- src/component/group/card/GroupCardV2.js
+++ src/component/group/card/GroupCardV2.js
@@ -5,7 +5,7 @@ export default (props) => {
 
     return (
         <div className="col">
-            <Card sx={{borderRadius:5}}
+            <Card sx={{ borderRadius: 5 }}
                 onClick={() => props.handleGroupCard(props.group)}>
                 <CardActionArea>
                     <CardMedia
@@ -17,37 +17,22 @@ export default (props) => {
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
                             {props.group.group_name}
-                            /
+                            |
+                            {props.group.project_name}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
                             {props.group.manager}
                         </Typography>
+
                         <Typography variant="body2" color="text.secondary">
-                        (디자인 전면 수정 예정임)(디자인 전면 수정 예정임)(디자인 전면 수정 예정임)(디자인 전면 수정 예정임)(디자인 전면 수정 예정임)
+                            {props.group.short_description}
+                        </Typography>
+                        <Typography>
+                            <TechStack tech_stack={props.group.tech_stack} />
                         </Typography>
                     </CardContent>
                 </CardActionArea>
             </Card>
-            {/* <a href="#" className="text-decoration-none text-dark" onClick={() => {
-                props.handleGroupCard(props.group)
-            }}>
-                <div className="card p-3 rounded-4">
-                    <div className="row">
-                        <div className="text-center col-xxl-4">
-                            <svg className="img-thumbnail rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-                            <div>
-                                <b>{props.group.group_name}</b>
-                            </div>
-                            <div>{props.group.manager}</div>
-                        </div>
-                        <div className="col-xxl-8 py-xxl-3">
-                            <h4>{props.group.project_name}</h4>
-                            <p>{props.group.short_description}</p>
-                            <div>
-                                <TechStack tech_stack={props.group.tech_stack} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a> */}
         </div>
     )
 }
