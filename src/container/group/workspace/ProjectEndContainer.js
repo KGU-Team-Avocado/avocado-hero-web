d2f406d30deda57ce597fd47ab0fde08908e970d
soyoung125 <sypark041754@gmail.com>
2022-10-03 23:39:12 +0900

    워크스페이스 종료 페이지 mui로 변경

--- src/container/group/workspace/ProjectEndContainer.js
+++ src/container/group/workspace/ProjectEndContainer.js
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle, Box, Button, Divider, Grid, Typography } from "@mui/material";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -19,18 +20,26 @@ const ProjectEndContainer = () => {
     }
     return (
         <>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 className="h2">프로젝트 종료</h1>
-            </div>
-            <div class="alert alert-success" role="alert">
-                <h4 class="alert-heading">프로젝트를 종료하시겠습니까?</h4>
-                <p>프로젝트 종료 후에는 프로젝트 내용을 수정할 수 없으며, 팀원 상대평가가 이뤄집니다.</p>
-                <hr />
-                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-                    <p class="mb-0">종료를 원하시면 우측 버튼을 눌러주세요.</p>
-                    <button type="button" className="btn btn-danger" onClick={() => endProject()}>종료</button>
-                </div>
-            </div>
+            <Grid container columnSpacing={2}>
+                <Grid display="flex" justifyContent="start" alignItems="center">
+                    <Typography variant="h3" mx={2}>
+                        신청자 목록
+                    </Typography>
+                </Grid>
+            </Grid>
+
+            <Divider sx={{ border: 1 }} />
+
+            <Box mt={2}>
+                <Alert severity="warning" action={
+                    <Button color="error" size="large" onClick={() => endProject()}>
+                        종료
+                    </Button>
+                }>
+                    <Typography variant="h6" gutterBottom >프로젝트를 종료하시겠습니까?</Typography>
+                    <Typography variant="body1" >프로젝트 종료 후에는 프로젝트 내용을 수정할 수 없으며, <strong>팀원 상대평가</strong>가 이뤄집니다. 종료를 원하시면 우측의 <strong>'종료'</strong> 버튼을 눌러주세요.</Typography>
+                </Alert>
+            </Box>
         </> 
     )
 }
