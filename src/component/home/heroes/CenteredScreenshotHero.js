0b4f2185aaabff1e882042a3b098832e4b72611e
wwwls99 <201810806@kyonggi.ac.kr>
2022-10-06 00:47:57 +0900

    대문 문구 수정

--- src/component/home/heroes/CenteredScreenshotHero.js
+++ src/component/home/heroes/CenteredScreenshotHero.js
@@ -1,13 +1,15 @@
+import MKButton from "component/common/mui-components/MKButton"
+
 export default () => {
     return (
         <>
             <div className="px-4 pt-5 my-5 text-center border-bottom">
-                <h1 className="display-4 fw-bold">Centered screenshot</h1>
+                <h1 className="display-4 fw-bold">아직도 팀플 조를 랜덤으로 구성하시나요?</h1>
                 <div className="col-lg-10 mx-auto">
-                    <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <p className="lead mb-4">원하는 사람들과 만나 팀을 구성하고 팀플 워크스페이스를 활용하여... 가나다라마바사아자차카타파하</p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                        <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
-                        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                        <MKButton variant="contained" color="warning" size="large">팀플 워크스페이스 구경하기</MKButton>
+                        <MKButton variant="outlined" color="dark" size="large">Secondary</MKButton>
                     </div>
                 </div>
                 <div className="overflow-hidden" style={{"maxHeight": "30vh"}}>
