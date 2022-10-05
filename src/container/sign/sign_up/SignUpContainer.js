5cbf672619e5beb6f46ab0f3d7cf58833ab472c6
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-10-05 12:55:24 +0900

    회원가입 mui로 갈아 엎으면서 기존 로직 전면 개편 (최신es문법 적용)

--- src/container/sign/sign_up/SignUpContainer.js
+++ src/container/sign/sign_up/SignUpContainer.js
@@ -1,221 +1,181 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import div from "react-bootstrap/Container";
 import axios from "axios";
 
 import { useRef, useState } from "react";
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, IconButton, InputAdornment, InputLabel, OutlinedInput, Radio, RadioGroup, Stack, TextField, Typography } from "@mui/material";
+import MKButton from "component/common/mui-components/MKButton";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const SignUpContainer = () => {
-  const UserIDInput = useRef();
-  const PasswordInput = useRef();
-  const ConfirmPasswordInput = useRef();
-  const UserNameInput = useRef();
-  const EmailInput = useRef();
 
-  const [userID, setUserId] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [type, setType] = useState("개인");
+  let navigate = useNavigate();
 
-  //const [checkError, setCheckError] = useState("");
+  const [values, setValues] = useState({
+    id: '',
+    password: '',
+    confirmPassword: '',
+    userName: '',
+    email: '',
+    type: '개인',
+  });
+  const [showPassword, setShowPassword] = useState(false);
 
-  const onUserIdHandler = (e) => {
-    setUserId(e.currentTarget.value);
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
   };
 
-  const onPasswordHandler = (e) => {
-    setPassword(e.currentTarget.value);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
-  const onConfirmPasswordHandler = (e) => {
-    setConfirmPassword(e.currentTarget.value);
-  };
-
-  const onUserNameHandler = (e) => {
-    setUserName(e.currentTarget.value);
-  };
-
-  const onEmailHandler = (e) => {
-    setEmail(e.currentTarget.value);
-  };
-
-  const handleRadiobutton = (e) => {
-    setType(e.target.value);
-  };
-
-  const onClick = (e) => {
-    e.preventDefault();
-
-    if (userID.length < 1) {
-      UserIDInput.current.focus();
-      return;
-    } else if (password.length < 1) {
-      PasswordInput.current.focus();
-      return;
-    } else if (confirmPassword.length < 1) {
-      ConfirmPasswordInput.current.focus();
-      return;
-    } else if (userName.length < 1) {
-      UserNameInput.current.focus();
-      return;
-    } else if (email.length < 1) {
-      EmailInput.current.focus();
-      return;
-    } else {
-      if (password !== confirmPassword) {
-        return alert("비밀번호 확인이 일치하지 않습니다.");
-      }
-
-      console.log(userID + password + confirmPassword + userName + email);
+  const signUp = () => {
+    console.log(JSON.stringify(values));
+    if (Object.values(values).every((val) => val.length > 0)) {
+      // 회원가입 허용
+      alert('회원가입 시켜줄게')
       axios
         .post("/usersRouter/register", {
-          user_id: userID,
-          user_password: password,
-          user_name: userName,
-          user_email: email,
-          user_type: type,
+          user_id: values.id,
+          user_password: values.password,
+          user_name: values.userName,
+          user_email: values.email,
+          user_type: values.type,
         })
         .then((response) => {
           console.log(response);
           if (response.data.idCheck === false) {
             alert("이미 사용중인 아이디입니다.");
-            //setCheckError("이미 사용중인 아이디입니다");
           }
           if (response.data.success === true) {
-            window.location.href = "/";
+            navigate('/')
+            // window.location.href = "/";
           }
         })
         .catch(function (error) {
           console.log(error);
         });
+
     }
-  };
+    else {
+      // 다시 입력하라고 난리
+      alert('안돼 돌아가')
+    }
+  }
 
   return (
-    <div className="d-flex justify-content-center pt-5">
-      <div
-        className="card p-5 w-100"
-        style={{ maxWidth: "500px", minWidth: "300px" }}
+    <>
+      <Stack
+        direction="row"
+        justifyContent="center"
       >
-        <h1>회원가입</h1>
-        <div className="py-3">
-          <Form>
-            <Form.Group
-              as={Row}
-              className="mb-3"
-              controlId="formPlaintextPassword"
-            >
-              <Col sm>
-                <Form.Control
-                  name="userID"
-                  type="text"
-                  placeholder="아이디"
-                  value={userID}
-                  ref={UserIDInput}
-                  onChange={onUserIdHandler}
+        <Card sx={{
+          maxWidth: 500,
+          width: "100%",
+          py: 5,
+          px: {
+            xs: 1,
+            sm: 2,
+            md: 5,
+          }
+        }}>
+          <CardContent>
+            <Typography variant="h2">회원가입</Typography>
+            <Stack my={3} spacing={1}>
+              {/* id */}
+              <TextField
+                id="outlined-basic-id"
+                label="아이디"
+                variant="outlined"
+                onChange={handleChange('id')}
+              />
+              {/* pw */}
+              <FormControl sx={{}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">비밀번호</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
                 />
-              </Col>
-              {/* <span id="checkMess">{checkError}</span> */}
-              {/* <Form.Text className="text-muted">{checkError}</Form.Text> */}
-            </Form.Group>
-
-            <Form.Group
-              as={Row}
-              className="mb-3"
-              controlId="formPlaintextPassword"
-            >
-              <Col sm>
-                <Form.Control
-                  name="password"
-                  type="password"
-                  placeholder="비밀번호"
-                  value={password}
-                  ref={PasswordInput}
-                  onChange={onPasswordHandler}
+              </FormControl>
+              {/* confirmPassword */}
+              <FormControl sx={{}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">비밀번호 확인</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-confirm-password"
+                  type={showPassword ? 'text' : 'password'}
+                  value={values.confirmPassword}
+                  onChange={handleChange('confirmPassword')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
                 />
-              </Col>
-            </Form.Group>
-
-            <Form.Group
-              as={Row}
-              className="mb-3"
-              controlId="formPlaintextPassword"
-            >
-              <Col sm>
-                <Form.Control
-                  name="confirmPassword"
-                  type="password"
-                  placeholder="비밀번호 확인"
-                  value={confirmPassword}
-                  ref={ConfirmPasswordInput}
-                  onChange={onConfirmPasswordHandler}
-                />
-              </Col>
-            </Form.Group>
-
-            <Form.Group
-              as={Row}
-              className="mb-3"
-              controlId="formPlaintextPassword"
-            >
-              <Col sm>
-                <Form.Control
-                  name="userName"
-                  type="text"
-                  placeholder="이름"
-                  value={userName}
-                  ref={UserNameInput}
-                  onChange={onUserNameHandler}
-                />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} className="mb-3" controlId="formBasicEmail">
-              <Col sm>
-                <Form.Control
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  ref={EmailInput}
-                  onChange={onEmailHandler}
-                />
-              </Col>
-            </Form.Group>
-            <Form.Group>
-              <label>
-                <input
-                  type="radio"
-                  value="개인"
-                  checked={type === "개인"}
-                  onClick={handleRadiobutton}
-                />
-                개인
-              </label>
-              <label className="form-check form-check-inline">
-                <input
-                  type="radio"
-                  value="기업"
-                  checked={type === "기업"}
-                  onClick={handleRadiobutton}
-                />
-                기업
-              </label>
-            </Form.Group>
-            <br />
-            <div className="d-grid gap-1">
-              <Button variant="secondary" type="submit" onClick={onClick}>
+              </FormControl>
+              {/* userName */}
+              <TextField
+                id="outlined-basic-name"
+                label="이름"
+                variant="outlined"
+                onChange={handleChange('userName')}
+              />
+              {/* email */}
+              <TextField
+                id="outlined-basic-email"
+                label="이메일"
+                variant="outlined"
+                onChange={handleChange('email')}
+              />
+              {/* type */}
+              <FormControl>
+                <FormLabel id="demo-controlled-radio-buttons-group">회원유형</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-controlled-radio-buttons-group"
+                  name="controlled-radio-buttons-group"
+                  value={values.type}
+                  onChange={handleChange('type')}
+                >
+                  <FormControlLabel value="개인" control={<Radio />} label="개인" />
+                  <FormControlLabel value="기업" control={<Radio />} label="기업" />
+                </RadioGroup>
+              </FormControl>
+              <MKButton
+                color="success"
+                variant="contained"
+                size="large"
+                onClick={() => signUp()}
+              >
                 회원가입
-              </Button>
-            </div>
-          </Form>
-        </div>
-      </div>
-    </div>
+              </MKButton>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Stack>
+
+    </>
   );
 };
 
