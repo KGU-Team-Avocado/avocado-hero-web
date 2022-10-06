52b1faa131bd1709b1dd65468215bebf42791b30
seeun01 <ssky601@gmail.com>
2022-10-06 14:49:28 +0900

    일자리 찾기 대문 문구 수정

--- src/component/home/heroes/ResponsiveLeftAlignedHero.js
+++ src/component/home/heroes/ResponsiveLeftAlignedHero.js
@@ -1,3 +1,5 @@
+import { useHref } from "react-router-dom"
+
 export default () => {
     return (
         <div className="container col-xxl-10 px-4 py-5">
@@ -6,10 +8,10 @@ export default () => {
                     <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
                 </div>
                 <div className="col-lg-6">
-                    <h1 className="display-5 fw-bold lh-1 mb-3">프로젝트 같이 할 동료를 찾아요</h1>
-                    <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <h1 className="display-5 fw-bold lh-1 mb-3">당신과 딱 맞는 회사를 찾아보세요!</h1>
+                    <p className="lead">히어로가 일자리를 소개합니다. 당신과 딱 맞는 일자리를 찾아보고 당신의 포토폴리오로 당신이 인재임을 증명해보세요!</p>
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                        <button type="button" className="btn btn-primary btn-lg px-4 me-md-2" onClick={()=> window.location.href = "/jobFinder"}>일자리 찾기</button>
                         <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
                     </div>
                 </div>
