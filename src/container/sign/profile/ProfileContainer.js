4221c81e1d8b5a7c9851388774a990daafaa33a4
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-11 01:18:21 +0900

    프로필 mui 이식

--- src/container/sign/profile/ProfileContainer.js
+++ src/container/sign/profile/ProfileContainer.js
@@ -14,73 +14,96 @@ import * as API from "../../../api/API"
 import { useSelector } from "react-redux";
 import { selectUser } from "api/redux/user/userSlice";
 import MKButton from "component/common/mui-components/MKButton";
+import { Alert, Box, CircularProgress, Collapse, IconButton, Stack, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 const ProfileContainer = () => {
+    const navigate = useNavigate();
     const params = useParams(); //url로 넘어온 파라미터를 받는 역할 (App.js 의 :id 참고)
     const user = useSelector(selectUser);
-    const user_id = params.id; //(params의 :id를 받는 역할)
+    const profile_id = params.id; //(params의 :id를 받는 역할)
     const [profile, setProfile] = useState(null);
-
-    const navigate = useNavigate();
-
-    console.log(`user_id : ${user_id}`);
+    const [isLoaded, setLoaded] = useState(false);
+    const [alertOpen, setAlertOpen] = useState(true);
 
     useEffect(() => {
-        getAndSetUserProfile(user_id);
-    }, [user_id]);
+        getAndSetUserProfile(profile_id);
+    }, [profile_id]);
 
-    const getAndSetUserProfile = async (user_id) => {
-        const temp = await API.findOneUserByUserId(user_id)
-        setProfile(temp);
+    const getAndSetUserProfile = async (profile_id) => {
+        const temp = await API.findOneUserByUserId(profile_id)
+        await setProfile(temp);
+        setLoaded(true);
     }
 
     return (
         <>
-            {/* 네비게이션 바로 나눌까 */}
-            {/* 
-            <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">프로필</Nav.Link>
-            <Nav.Link href="./ProfileGroup">그룹</Nav.Link>
-            <Nav.Link href="#pricing">포트폴리오</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> 
-      */}
             {
-                profile ?
-                    <>
-                        <h3><b>{profile.user_id}</b>의 프로필</h3>
-                        <div className="alert alert-warning alert-dismissible fade show" role="alert">
-                            <div>멋진 프로필을 작성하면 기업이 당신을 스카우트할 수 있습니다.
-                                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>
-                        </div>
-                        
-                        {
-                            // 위치를 상단 프로필이랑 justify-content-between으로 적용하기
-                            user?.user_id === profile.user_id
-                                ?
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-<MKButton variant="contained" color="info" onClick={()=>navigate(`/user/ProfileUpdate/${profile.user_id}`)} style={{
-
-}}>프로필 수정</MKButton>
-                                </div>
-                                :
-                                <></>
-                        }
-
-                        <ProfileCard
-                            profile={profile}
-                        />
-                    
-                    </>
+                isLoaded
+                    ?
+                    (
+                        profile ?
+                            <>
+                                {
+                                    user?.user_id === profile.user_id
+                                        ?
+                                        <Collapse in={alertOpen}>
+                                            <Alert
+                                                action={
+                                                    <IconButton
+                                                        aria-label="close"
+                                                        color="inherit"
+                                                        size="small"
+                                                        onClick={() => {
+                                                            setAlertOpen(false);
+                                                        }}
+                                                    >
+                                                        <CloseIcon fontSize="inherit" />
+                                                    </IconButton>
+                                                }
+                                                sx={{ mb: 2 }}
+                                            >
+                                                멋진 프로필을 작성하면 기업이 당신을 스카우트할 수 있습니다.
+                                            </Alert>
+                                        </Collapse>
+                                        :
+                                        <></>
+                                }
+                                <Box
+                                    width="100%"
+                                    display="flex"
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                    mb={5}
+                                >
+                                    <Typography variant="h3">
+                                        {profile.user_id}의 프로필
+                                    </Typography>
+                                    {
+                                        user?.user_id === profile.user_id
+                                            ?
+                                            <MKButton
+                                                variant="contained"
+                                                color="info"
+                                                onClick={() => navigate(`/user/ProfileUpdate/${profile.user_id}`)}
+                                            >
+                                                프로필 수정
+                                            </MKButton>
+                                            :
+                                            <></>
+                                    }
+                                </Box>
+                                <ProfileCard
+                                    profile={profile}
+                                />
+                            </>
+                            :
+                            <Typography>누구쎄용?</Typography>
+                    )
                     :
-                    <>
-                        <div>누구쎄용?</div>
-                    </>
+                    <Box sx={{ alignItems: 'center', display: 'flex', minHeight:'500px', justifyContent: 'center', width:'100%' }}>
+                        <CircularProgress />
+                    </Box>
             }
         </>
     )
