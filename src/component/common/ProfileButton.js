f209480f03e2b52544c705ac88643b31ddff0dc9
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-19 00:20:02 +0900

    프로필 버튼을 모듈화 함

---
+++ src/component/common/ProfileButton.js
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import MKButton from "./mui-components/MKButton"
+import * as API from '../../api/API';
+import { useSelector } from "react-redux";
+import { selectUser } from "api/redux/user/userSlice";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Menu, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default ({
+    open,
+    anchorEl,
+    setAnchorEl
+}) => {
+
+    const navigate = useNavigate();
+    const userInfo = useSelector(selectUser);
+    const [uploadedImage, setUploadedImage] = useState(null);
+    const setProfileImage = async () => {
+        setUploadedImage(await API.fetchImage(userInfo?.user_id)); //프로필 이미지 불러오는 코드
+    }
+
+    useEffect(() => {
+        setProfileImage();
+    }, [userInfo]);
+
+    const handleClose = (url) => {
+        navigate(url);
+        setAnchorEl(null);
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const logout = () => {
+        sessionStorage.clear();
+        window.location.href = ''
+    };
+
+    return (
+        <>
+            <MKButton
+                variant="outline"
+                // color="dark"
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                {
+                    uploadedImage
+                        ?
+                        <img
+                            src={uploadedImage}
+                            width="32"
+                            height="32"
+                            className="rounded-circle mx-1"
+                        />
+                        :
+                        <svg
+                            className="img-thumbnail rounded-circle"
+                            width="32"
+                            height="32"
+                            xmlns="http://www.w3.org/2000/svg"
+                            role="img"
+                            aria-label="Placeholder: 140x140"
+                            preserveAspectRatio="xMidYMid slice"
+                            focusable="false"
+                        >
+                            <rect width="100%" height="100%" fill="#777" />
+                        </svg>
+                }
+                <div className="mx-1">{userInfo.user_id}</div>
+                <ArrowDropDownIcon />
+            </MKButton>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={() => handleClose(`/user/${userInfo.user_id}`)}>프로필</MenuItem>
+                <MenuItem onClick={() => handleClose(`myWorkspace`)}>내 워크스페이스</MenuItem>
+                <MenuItem onClick={() => logout()}>로그아웃</MenuItem>
+            </Menu>
+        </>
+    )
+}
--- src/component/common/layouts/Navbars/DefaultNavbar/index.js
+++ src/component/common/layouts/Navbars/DefaultNavbar/index.js
@@ -45,20 +45,13 @@ import MKButton from "component/common/mui-components/MKButton";
 import { useSelector } from "react-redux";
 import { selectUser } from "api/redux/user/userSlice";
 import * as API from '../../../../../api/API';
+import ProfileButton from "component/common/ProfileButton";
 
 function DefaultNavbar({ brand, routes, transparent, light, action, sticky, relative, center }) {
 
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = (url) => {
-    navigate(url);
-    setAnchorEl(null);
-  };
-
   const userInfo = useSelector(selectUser);
 
   const logout = () => {
@@ -78,11 +71,6 @@ function DefaultNavbar({ brand, routes, transparent, light, action, sticky, rela
   const [mobileView, setMobileView] = useState(false);
 
   const openMobileNavbar = () => setMobileNavbar(!mobileNavbar);
-
-  const [uploadedImage, setUploadedImage] = useState(null);
-  const setProfileImage = async () => {
-    setUploadedImage(await API.fetchImage(userInfo?.user_id)); //프로필 이미지 불러오는 코드
-  }
 
   useEffect(() => {
 
@@ -109,10 +97,6 @@ function DefaultNavbar({ brand, routes, transparent, light, action, sticky, rela
     // Remove event listener on cleanup
     return () => window.removeEventListener("resize", displayMobileNavbar);
   }, []);
-
-  useEffect(()=>{
-    setProfileImage();
-  },[userInfo]);
 
   const renderNavbarItems = routes.map(({ name, icon, href, route, collapse }) => (
     <DefaultNavbarDropdown
@@ -528,54 +512,12 @@ function DefaultNavbar({ brand, routes, transparent, light, action, sticky, rela
               // 로그인 했을 때를 true, 안했을 때를 false로 관리해주세요
               userInfo ? (
                 <>
-                  <MKButton
-                    variant="outline"
-                    // color="dark"
-                    id="basic-button"
-                    aria-controls={open ? 'basic-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={handleClick}
-                  >
-                    {
-                      uploadedImage
-                        ?
-                        <img
-                          src={uploadedImage}
-                          width="32"
-                          height="32"
-                          className="rounded-circle mx-1"
-                        />
-                        :
-                        <svg
-                          className="img-thumbnail rounded-circle"
-                          width="32"
-                          height="32"
-                          xmlns="http://www.w3.org/2000/svg"
-                          role="img"
-                          aria-label="Placeholder: 140x140"
-                          preserveAspectRatio="xMidYMid slice"
-                          focusable="false"
-                        >
-                          <rect width="100%" height="100%" fill="#777" />
-                        </svg>
-                    }
-                    <div className="mx-1">{userInfo.user_id}</div>
-                    <ArrowDropDownIcon />
-                  </MKButton>
-                  <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
+                  <ProfileButton
                     open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                      'aria-labelledby': 'basic-button',
-                    }}
-                  >
-                    <MenuItem onClick={() => handleClose(`/user/${userInfo.user_id}`)}>프로필</MenuItem>
-                    <MenuItem onClick={() => handleClose(`myWorkspace`)}>내 워크스페이스</MenuItem>
-                    <MenuItem onClick={() => logout()}>로그아웃</MenuItem>
-                  </Menu>
+                    // handleClick={handleClick}
+                    anchorEl={anchorEl}
+                    setAnchorEl={setAnchorEl}
+                  />
                 </>
               ) : (
                 <MKButton color="success" onClick={() => navigate('/signin')}>로그인</MKButton>
