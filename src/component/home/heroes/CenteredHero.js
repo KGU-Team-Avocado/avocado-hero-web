aae52cf2673ebfffc1a0978a144594a0bb232007
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-18 13:35:39 +0900

    회원가입 버그 수정, 메인 버튼 레이아웃 조정

--- src/component/home/heroes/CenteredHero.js
+++ src/component/home/heroes/CenteredHero.js
@@ -24,7 +24,20 @@ export default ({navigate}) => {
             >
                 <Typography variant="h1">포트폴리오, 더이상 혼자 고민하지 마세요!</Typography>
                 <Typography>포트폴리오 관리에 어려움이 있으신가요? 히어로에서 활동하시면 여러분의 활동 기록을 자동으로 상세하게 관리해드립니다!</Typography>
-                <Stack align="center" direction="row" justifyContent="center" spacing={3}>
+                <Stack
+                    direction={{ xs: "column", sm: "row" }}
+                    spacing={1}
+                    justifyContent="center"
+                    sx={{
+                        xs: {
+                            display: 'grid',
+                        },
+                        sm: {
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }
+                    }}
+                >
                     <MKButton variant="contained" color="success" size="large" onClick={()=>navigate('/user/gabrielyoon7')}>포트폴리오 예제 구경하기</MKButton>
                     <MKButton variant="outlined" color="secondary" size="large" onClick={()=>navigate('/signup')}>회원가입 하기</MKButton>
                 </Stack>
