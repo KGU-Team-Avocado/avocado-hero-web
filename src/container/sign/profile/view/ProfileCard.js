f9f31b372a4e48f15d4c7c738b8cce331b175795
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-28 19:20:53 +0900

    프로필 카드 디자인 개선 및 Responsive Card 제작

--- src/container/sign/profile/view/ProfileCard.js
+++ src/container/sign/profile/view/ProfileCard.js
@@ -1,4 +1,5 @@
 import { Card, CardActionArea, CardContent, Grid, Stack, Typography } from "@mui/material";
+import ResponsiveCard from "component/common/ResponsiveCard";
 import TechStack from "component/common/TechStack";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -22,83 +23,70 @@ export default (props) => {
     return (
         <>
             <Grid container spacing={1}>
-                <Grid item xxl={3} sx={{width:'100%'}}>
+                <Grid item xxl={3} sx={{ width: '100%' }}>
                     <Stack spacing={1}>
-                        <Card>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Avatar user_id={profile?.user_id} imgURL={profile?.imgURL}/>
-                                    <Typography>
-                                        {profile?.nickname}
-                                    </Typography>
-                                    <Typography>
-                                        {profile?.name}
-                                    </Typography>
-                                    <Typography>
-                                        {profile?.email}
-                                    </Typography>
-                                    <Typography>
-                                        {profile?.phoneNumber}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                        <Card>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography>
-                                        한줄 소개
-                                    </Typography>
-                                    <Typography>
-                                        {profile?.introduceOne}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
+                        <ResponsiveCard>
+                            <Avatar user_id={profile?.user_id} imgURL={profile?.imgURL} />
+                            <Typography variant="inherit">
+                                {profile?.nickname}
+                            </Typography>
+                            <Typography variant="h3">
+                                {profile?.name}
+                            </Typography>
+                            <Typography variant="h5">
+                                {profile?.user_id}
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                {profile?.email}
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                {profile?.phoneNumber}
+                            </Typography>
+                        </ResponsiveCard>
+                        <ResponsiveCard>
+                            <Typography variant="h5">
+                                한줄 소개
+                            </Typography>
+                            <Typography>
+                                {profile?.introduceOne}
+                            </Typography>
+                        </ResponsiveCard>
                     </Stack>
                 </Grid>
-                <Grid item xxl={9} sx={{width:'100%'}}>
+                <Grid item xxl={9} sx={{ width: '100%' }}>
                     <Stack spacing={1}>
-                        <Card>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography>기본 정보</Typography>
-                                    <Typography>소속</Typography>
-                                    <Typography>
-                                        {/* {
+                        <ResponsiveCard>
+                            <Typography variant="h3">기본 정보</Typography>
+                            <Typography variant="h4">소속</Typography>
+                            <Typography>
+                                {/* {
                                                 belongs.map(belong => (<Map mapper={belong} />))
                                             } */}
-                                        {profile && profile?.belongs}
-                                    </Typography>
-                                    <Typography>분야</Typography>
-                                    <Typography>{profile && <TechStack tech_stack={profile?.fields} />}</Typography>
-                                    <Typography>링크</Typography>
-                                    <Typography>
-                                        <a href={profile && profile?.links}>{profile && profile?.links}</a>
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                        <Card>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography>세부 정보</Typography>
-                                    <Typography>키워드</Typography>
-                                    <Typography>{profile && <TechStack tech_stack={profile?.keywords} />}</Typography>
-                                    <Typography>성향</Typography>
-                                    <Typography>
-                                        {
-                                            profile &&
-                                            <TechStack
-                                                tech_stack={profile.personalities}
-                                            />
-                                        }
-                                    </Typography>
-                                    <Typography>소개글</Typography>
-                                    <Typography>{profile && profile?.introduce}</Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
+                                {profile && profile?.belongs}
+                            </Typography>
+                            <Typography variant="h4">분야</Typography>
+                            <Typography>{profile && <TechStack tech_stack={profile?.fields} />}</Typography>
+                            <Typography variant="h4">링크</Typography>
+                            <Typography>
+                                <a href={profile && profile?.links}>{profile && profile?.links}</a>
+                            </Typography>
+                        </ResponsiveCard>
+                        <ResponsiveCard>
+                            <Typography variant="h3">세부 정보</Typography>
+                            <Typography variant="h4">키워드</Typography>
+                            <Typography>{profile && <TechStack tech_stack={profile?.keywords} />}</Typography>
+                            <Typography variant="h4">성향</Typography>
+                            <Typography>
+                                {
+                                    profile &&
+                                    <TechStack
+                                        tech_stack={profile.personalities}
+                                    />
+                                }
+                            </Typography>
+                            <Typography variant="h4">소개글</Typography>
+                            <Typography>{profile && profile?.introduce}</Typography>
+                        </ResponsiveCard>
                     </Stack>
                 </Grid>
             </Grid>
