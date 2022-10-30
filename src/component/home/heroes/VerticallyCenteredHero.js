5ad8a1e537a0f93bbfd317ff73dddeee0967eb3a
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-30 17:00:08 +0900

    mui 이식

--- src/component/home/heroes/VerticallyCenteredHero.js
+++ src/component/home/heroes/VerticallyCenteredHero.js
@@ -1,22 +1,33 @@
+import { Box, Container, Grid, Stack, Typography } from "@mui/material"
 import SignIn from "container/sign/sign_in/SignIn"
 
 export default () => {
     return (
-        <div className="container col-xl-10 col-xxl-10 px-4 py-5">
-            <div className="row align-items-center g-lg-5 py-5">
-                <div className="col-lg-7 text-center text-lg-start">
-                    <h1 className="display-4 fw-bold lh-1 mb-3">히어로, 오늘 팀 일정 알려줘</h1>
-                    <p className="col-lg-10 fs-6">
-                    더이상의 왔다갔다 시간낭비는 NO!! 팀 일정을 오직 한 곳에서 관리하세요<br />
-                    팀 전체 공유 일정을 통해 팀원 간의 불필요한 소통을 줄이세요<br />
-                    예정된 데드라인 등 중요한 날짜를 다시는 놓치지 마세요<br />
-                    중요한 정보를 정리하고 체계화해 관리해 보세요
-                    </p>
-                </div>
-                <div className="col-md-10 mx-auto col-lg-5">
-                    <SignIn/>
-                </div>
-            </div>
-        </div>
+        <>
+            <Container maxWidth="sm">
+                <Stack
+                    my={5}
+                    px={4}
+                    spacing={3}
+                >
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={12} lg={7}>
+                            <Typography variant="h1">히어로, 오늘 팀 일정 알려줘</Typography>
+                            <Typography>
+                                더이상의 왔다갔다 시간낭비는 NO!! 팀 일정을 오직 한 곳에서 관리하세요
+                                팀 전체 공유 일정을 통해 팀원 간의 불필요한 소통을 줄이세요
+                                예정된 데드라인 등 중요한 날짜를 다시는 놓치지 마세요
+                                중요한 정보를 정리하고 체계화해 관리해 보세요
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} lg={5}>
+                            <Box maxWidth={400}>
+                                <SignIn />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Stack>
+            </Container>
+        </>
     )
 }
