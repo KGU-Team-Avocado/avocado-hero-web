f66664c4e86752300fb8993b200a69d36dc1dcee
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-01 21:56:49 +0900

    avatar 이미지 오류 핸들링

--- src/container/sign/profile/avatar/Avatar.js
+++ src/container/sign/profile/avatar/Avatar.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import AvatarEditButton from "./AvatarEditButton";
 import * as API from '../../../../api/API';
+import defaultImage from '../../../../assets/img/logo512.png';
 
 export default ({ user_id, imgURL, edit }) => {
     const [uploadedImage, setUploadedImage] = useState(null);
@@ -13,35 +14,22 @@ export default ({ user_id, imgURL, edit }) => {
         setUploadedImage(await API.fetchProfileImage(imgURL)); //프로필 이미지 불러오는 코드
     }
 
+    const handleImgError = (e) => {
+        e.target.src = defaultImage;
+    }
+
     return (
         <>
             <div class="itemCenter">
+                <img
+                    className="border rounded-circle"
+                    width="140"
+                    height="140"
+                    src={uploadedImage}
+                    onError={handleImgError}
+                />
                 {
-                    uploadedImage ?
-                        <img
-                            className="border rounded-circle"
-                            width="140"
-                            height="140"
-                            src={uploadedImage}
-                            alt="profile"
-                        />
-                        :
-                        <svg
-                            className="img-thumbnail rounded-circle"
-                            width="140"
-                            height="140"
-                            xmlns="http://www.w3.org/2000/svg"
-                            role="img"
-                            aria-label="Placeholder: 140x140"
-                            preserveAspectRatio="xMidYMid slice"
-                            focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#777" />
-                            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                        </svg>
-                }
-                {
-                    edit && <AvatarEditButton setUploadedImage={setUploadedImage}/>
+                    edit && <AvatarEditButton setUploadedImage={setUploadedImage} />
                 }
             </div>
         </>
