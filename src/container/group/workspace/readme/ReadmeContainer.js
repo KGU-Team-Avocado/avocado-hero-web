6153636a65b51d96e4c48766e49f55c5068440cf
hido02 <bestdohee@gmail.com>
2022-11-06 22:44:20 +0900

    [리드미] (찔끔) 수정하기 버튼 수정해서 생성 open 함수 만드는중

--- src/container/group/workspace/readme/ReadmeContainer.js
+++ src/container/group/workspace/readme/ReadmeContainer.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import { Box, Button } from "@mui/material"
 import { selectGroup } from "api/redux/group/groupSlice";
 import WorkspaceHeader from "component/workspace/layout/WorkspaceHeader"
 import { useDispatch, useSelector } from "react-redux";
 import Readme from "./Readme"
+import ReadmeModal from "./ReadmeModal";
+import ModalStaticBackdrop from "component/common/modal/ModalStaticBackdrop";
 
 export default () => {
     const group = useSelector(selectGroup);
     const dispatch = useDispatch();
+
+    const [readMe, setReadme] = useState('');
+
+    const handleReadmeChange = event => {
+        setReadme(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const [readmeCreaeteModalOpen, setReadmeCreateModalOpen] = useState(false);
 
     return (
         <>
@@ -18,15 +30,21 @@ export default () => {
                         variant="outlined"
                         aria-label="upload picture"
                         component="label"
-                    //  onClick={() => setShow(true)} 
+                        onClick={() => setReadmeCreateModalOpen(true)}
                     >
-                        수정하기
+                        수정하기(리드미 작성)
                     </Button>
                 }
             />
             <Box>
-                <Readme group_id={group?._id}/>
+                <Readme group_id={group?._id} />
             </Box>
+            <ModalStaticBackdrop
+                keepMounted
+                width="md"
+                // open={readmeCreaeteModalOpen}
+                // component={<ReadmeModal readmeCreateModalOpen={readmeCreaeteModalOpen} setOpen={setReadmeCreateModalOpen} />}
+            />
         </>
     )
 }
