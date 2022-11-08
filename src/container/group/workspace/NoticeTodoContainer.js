f6e5fb512a163a727a02ec4b6acf72d7179ec580
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-09 00:17:53 +0900

    fix: 대시보드에서 반응형 grid가 적용이 안되던 문제를 수정

--- src/container/group/workspace/NoticeTodoContainer.js
+++ src/container/group/workspace/NoticeTodoContainer.js
@@ -1,25 +1,29 @@
-import { Box, Grid } from "@mui/material"
+import { Grid } from "@mui/material"
 import ResponsiveCard from "component/common/ResponsiveCard"
 import Notice from "./Notice"
 import Todo from "./Todo"
 
-export default () => {
-    // 이거 반응형 왜 안됨? 개인적으로는 WorkspaceLayout.js에서 설정해주는 것이 의심됨
-    
+const NoticeTodoContainer = () => {
+
     return (
-        <Box>
-            <Grid conatiner spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <ResponsiveCard>
-                        <Notice />
-                    </ResponsiveCard>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <ResponsiveCard>
-                        <Todo />
-                    </ResponsiveCard>
-                </Grid>
+        <Grid
+            container
+            rowSpacing={{ xs: 1, sm: 1, md: 2 }}
+            columnSpacing={{ sm: 1, md: 2 }}
+        >
+            <Grid item xs={12} md={6}>
+                <ResponsiveCard>
+                    <Notice />
+                </ResponsiveCard>
             </Grid>
-        </Box>
+            <Grid item xs={12} md={6}>
+                <ResponsiveCard>
+                    <Todo />
+                </ResponsiveCard>
+            </Grid>
+        </Grid>
     )
 }
+
+
+export default NoticeTodoContainer;
