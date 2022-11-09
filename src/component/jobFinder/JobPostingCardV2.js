2e5ca9f441741f3d1e6111265e5a8f0b87f93142
soyoung125 <sypark041754@gmail.com>
2022-11-09 23:32:22 +0900

    refactor: 불필요한 코드 제거

--- src/component/jobFinder/JobPostingCardV2.js
+++ src/component/jobFinder/JobPostingCardV2.js
@@ -1,8 +1,5 @@
 import { Avatar, Card, CardActionArea, CardActions, CardContent, CardHeader, CardMedia, Divider, IconButton, Stack, Typography } from "@mui/material"
 import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import TechStack from "component/common/TechStack";
 import defaultImage from '../../assets/img/logo512.png';
 import { useEffect, useState } from "react";
@@ -27,30 +24,16 @@ export default (props) => {
         if(postingImage !== null) {
             setUploadedPostingImage(postingImage);
         }
-        //setUploadedCompanyImage(await API.fetchCompanyImage(props.posting.company_image));
-        //setUploadedPostingImage(await API.fetchCompanyImage(props.posting.posting_image));
     }
 
-    const handleImgError = (e) => {
-        e.target.src = defaultImage;
-    }
     // 수정 예정
     return (
         <Card
             sx={{
                 my:2,
-                // backgroundColor:'#f3f3f3'
             }}
             onClick={() => props.handleClick(props.posting)}
         >
-            {/* <CardActionArea>
-                <CardContent>
-                <Typography variant="h2">{props.posting.name}</Typography>
-                <Typography variant="h3">{props.posting.title}</Typography>
-                <Typography variant="h5">{props.posting.field}</Typography>
-                <Typography variant="h6">{props.posting.period}</Typography>
-                </CardContent>
-            </CardActionArea> */}
             <CardHeader
                 avatar={
                     <Avatar aria-label="recipe" src={uploadedCompanyImage}  >
@@ -65,7 +48,9 @@ export default (props) => {
                 title={props.posting.name}
                 subheader={props.posting.job_tag}
             />
+
             <Divider sx={{ margin: 0 }}/>
+
             <CardMedia
                 component="img"
                 //height="194"
@@ -75,7 +60,9 @@ export default (props) => {
                     margin: 0
                 }}
             />
+
             <Divider sx={{ margin: 0 }}/>
+
             <CardContent sx={{ py: '8px' }}>
                 <Typography variant="h5">
                     {props.posting.title}
@@ -84,7 +71,9 @@ export default (props) => {
                     ~{props.posting.period}
                 </Typography>
             </CardContent>
+
             <Divider sx={{ margin: 0 }}/>
+            
             <CardActions disableSpacing sx={{ px: '24px' }}>
                 <TechStack tech_stack={props.posting.skill_tags} />
             </CardActions>
