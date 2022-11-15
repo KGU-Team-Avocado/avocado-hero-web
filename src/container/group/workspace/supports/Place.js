b72107d5c8076f45e077ea72e786f73968908dc2
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-15 14:33:26 +0900

    feat: 장소대여 기능에서 json array에 따라 카드 표시 기능 추가

--- src/container/group/workspace/supports/Place.js
+++ src/container/group/workspace/supports/Place.js
@@ -1,8 +1,28 @@
-import { Grid, Stack, Typography } from "@mui/material";
+import { Chip, Grid, Stack, Typography } from "@mui/material";
 import StoreIcon from '@mui/icons-material/Store';
 import ResponsiveCard from "component/common/ResponsiveCard";
 
 const Place = () => {
+
+    const places = [
+        {
+            _id: 0,
+            type: '모임장소',
+            title: '회의실',
+            description: '회의하세요',
+            address: '경기대학교 8강의동 창고',
+            cost: '시간당 8000원',
+        },
+        {
+            _id: 1,
+            type: '전시회',
+            title: '주차장 빌려드립니다.',
+            description: '바닥에서 전시를!',
+            address: '경기대학교 기숙사 앞 주차장',
+            cost: '시간당 384823원',
+        },
+    ];
+
     return (
         <Stack>
             <Typography variant="h3"><StoreIcon />장소대여</Typography>
@@ -11,18 +31,21 @@ const Place = () => {
                 rowSpacing={{ xs: 1, sm: 1, md: 2 }}
                 columnSpacing={{ sm: 1, md: 2 }}
             >
-                <Grid item xs={12} md={6}>
-                    <ResponsiveCard>
-                        <Typography>모임장소</Typography>
-                    </ResponsiveCard>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <ResponsiveCard>
-                        <Typography>전시회</Typography>
-                    </ResponsiveCard>
-                </Grid>
+                {
+                    places.map((place) =>
+                        <Grid item xs={12} md={6} key={place._id}>
+                            <ResponsiveCard>
+                                <Chip label={place.type}/>
+                                <Typography>{place.title}</Typography>
+                                <Typography>{place.description}</Typography>
+                                <Typography>{place.address}</Typography>
+                                <Typography>{place.cost}</Typography>
+                            </ResponsiveCard>
+                        </Grid>
+                    )
+                }
             </Grid>
         </Stack>
-    )
-}
+    );
+};
 export default Place;
