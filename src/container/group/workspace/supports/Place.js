12aae440782ea148fc98caa96e348d7e0dd69a16
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-15 16:00:15 +0900

    feat: 장소 대여 페이지 카드 디자인 구현

--- src/container/group/workspace/supports/Place.js
+++ src/container/group/workspace/supports/Place.js
@@ -1,4 +1,4 @@
-import { Chip, Grid, Stack, Typography } from "@mui/material";
+import { Button, Chip, Grid, Stack, Typography } from "@mui/material";
 import StoreIcon from '@mui/icons-material/Store';
 import ResponsiveCard from "component/common/ResponsiveCard";
 
@@ -11,7 +11,7 @@ const Place = () => {
             title: '회의실',
             description: '회의하세요',
             address: '경기대학교 8강의동 창고',
-            cost: '시간당 8000원',
+            cost: '8000원/시간',
         },
         {
             _id: 1,
@@ -19,13 +19,29 @@ const Place = () => {
             title: '주차장 빌려드립니다.',
             description: '바닥에서 전시를!',
             address: '경기대학교 기숙사 앞 주차장',
-            cost: '시간당 384823원',
+            cost: '384823원/1일',
+        },
+        {
+            _id: 2,
+            type: '전시회',
+            title: '주차장 빌려드립니다.',
+            description: '바닥에서 전시를!',
+            address: '경기대학교 기숙사 앞 주차장',
+            cost: '34453원/시간',
+        },
+        {
+            _id: 3,
+            type: '모임장소',
+            title: '스터디룸',
+            description: '시끄럽게 대화해도 됩니다.',
+            address: '우리집',
+            cost: '6,000원/시간',
         },
     ];
 
     return (
         <Stack>
-            <Typography variant="h3"><StoreIcon />장소대여</Typography>
+            <Typography variant="h5"><StoreIcon />장소대여</Typography>
             <Grid
                 container
                 rowSpacing={{ xs: 1, sm: 1, md: 2 }}
@@ -35,11 +51,19 @@ const Place = () => {
                     places.map((place) =>
                         <Grid item xs={12} md={6} key={place._id}>
                             <ResponsiveCard>
-                                <Chip label={place.type}/>
-                                <Typography>{place.title}</Typography>
-                                <Typography>{place.description}</Typography>
+                                <Chip label={place.type} />
+                                <Typography variant="h5">{place.title}</Typography>
+                                <Typography variant="h6">{place.description}</Typography>
                                 <Typography>{place.address}</Typography>
-                                <Typography>{place.cost}</Typography>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                    spacing={2}
+                                >
+                                    <Typography variant="caption">{place.cost}</Typography>
+                                    <Button variant="contained">신청하기</Button>
+                                </Stack>
                             </ResponsiveCard>
                         </Grid>
                     )
