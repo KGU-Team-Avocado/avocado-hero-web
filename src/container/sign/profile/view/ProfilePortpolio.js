96e0bf194eb41f3d835a89adb31f418877bbcd20
soyoung125 <sypark041754@gmail.com>
2022-11-17 22:22:52 +0900

    fix: 불필요한 코드 제거

--- src/container/sign/profile/view/ProfilePortpolio.js
+++ src/container/sign/profile/view/ProfilePortpolio.js
@@ -19,7 +19,6 @@ const ProfilePortpolio = () => {
     const [showEvalStatistics, setShowEvalStatistics] = useState(false)
 
     const setSelectedGroup = (group) => {
-        // alert(JSON.stringify(group))
         if (window.confirm(group.project_name + '으로 이동하시겠습니까?')) {
             window.location.href = "/project/" + group._id;
         }
@@ -66,138 +65,22 @@ const ProfilePortpolio = () => {
         }
     }
 
-    const goProjectList = () => {
-        navigate(`/groupFinder`);
-    }
-
     return (
         <>
             <div className="pt-3 pb-2 mb-3 border-bottom">
                 <h1 className="h2">통계</h1>
             </div>
 
-            {/* 그래프 */}
-            {/* <div style={{ width: '100%', height: '300px', margin: '0 auto' }}>
-                <ResponsiveBar
-                    data={[
-                        {
-                            "country": "ㅁㄴㅇ",
-                            "hot dog": 56,
-                            "hot dogColor": "hsl(246, 70%, 50%)",
-                            "burger": 110,
-                            "burgerColor": "hsl(300, 70%, 50%)",
-                            "sandwich": 26,
-                            "sandwichColor": "hsl(283, 70%, 50%)",
-                            "kebab": 48,
-                            "kebabColor": "hsl(310, 70%, 50%)",
-                            "fries": 110,
-                            "friesColor": "hsl(355, 70%, 50%)",
-                            "donut": 51,
-                            "donutColor": "hsl(8, 70%, 50%)"
-                        },
-                    ]}
-                    keys={[
-                        'hot dog',
-                        'burger',
-                        'sandwich',
-                        'kebab',
-                        'fries',
-                        'donut'
-                    ]}
-                    indexBy="country"
-                    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-                    padding={0.4}
-                    layout="horizontal"
-                    valueScale={{ type: 'linear' }}
-                    indexScale={{ type: 'band', round: true }}
-                    colors={{ scheme: 'nivo' }}
-                    borderColor={{
-                        from: 'color',
-                        modifiers: [
-                            [
-                                'darker',
-                                1.6
-                            ]
-                        ]
-                    }}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={{
-                        tickSize: 10,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: '참여 프로젝트',
-                        legendPosition: 'middle',
-                        legendOffset: 40
-                    }}
-                    axisLeft={null}
-                    labelSkipWidth={12}
-                    labelSkipHeight={12}
-                    labelTextColor={{
-                        from: 'color',
-                        modifiers: [
-                            [
-                                'darker',
-                                1.6
-                            ]
-                        ]
-                    }}
-                    legends={[
-                        {
-                            dataFrom: 'keys',
-                            anchor: 'bottom-right',
-                            direction: 'column',
-                            justify: false,
-                            translateX: 120,
-                            translateY: 0,
-                            itemsSpacing: 2,
-                            itemWidth: 100,
-                            itemHeight: 20,
-                            itemDirection: 'left-to-right',
-                            itemOpacity: 0.85,
-                            symbolSize: 20,
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemOpacity: 1
-                                    }
-                                }
-                            ]
-                        }
-                    ]}
-                    role="application"
-                    ariaLabel="Nivo bar chart demo"
-                    barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue; }}
-
-                />
-            </div> */}
-
-            {/* {showRoleStatistics ?
-                <ProfilePieChart data={roleStatistics} />
-                : <Alert action={
-                    <Button color="error" size="large" onClick={() => navigate(`/groupFinder`)}>
-                        프로젝트 리스트 보기
-                    </Button>} severity="info">역할 통계 데이터가 존재하지 않습니다. 프로젝트에 참여하여 새로운 역할을 받아보세요!</Alert>
-            }
-
-            <div style={{ width: '100%', height: 'auto', margin: '0 auto' }}>
-                {showEvalStatistics ?
-                    <ProfileRadar data={evalStatistics} user_id={user?.user_id} />
-                    : <Alert action={
-                        <Button color="error" size="large" onClick={() => navigate(`/myWorkspace`)}>
-                            내 워크스페이스 가기
-                        </Button>} severity="info">상호평가 통계 데이터가 존재하지 않습니다. 프로젝트 워크스페이스로 이동해 종료 프로젝트에서 상호평가를 진행해보세요!</Alert>
-                }
-            </div> */}
             <Grid container spacing={1}>
-                    {showRoleStatistics ?
-                        <Grid item sm={12} lg={6} minHeight={160}><ProfilePieChart data={roleStatistics} /></Grid>
-                        : <Grid xs={12}><Alert action={
-                            <Button color="error" size="large" onClick={() => navigate(`/groupFinder`)}>
-                                프로젝트 리스트 보기
-                            </Button>} severity="info">역할 통계 데이터가 존재하지 않습니다. 프로젝트에 참여하여 새로운 역할을 받아보세요!</Alert></Grid>
-                    }
+
+                {showRoleStatistics ?
+                    <Grid item sm={12} lg={6} minHeight={160}><ProfilePieChart data={roleStatistics} /></Grid>
+                    : <Grid xs={12}><Alert action={
+                        <Button color="error" size="large" onClick={() => navigate(`/groupFinder`)}>
+                            프로젝트 리스트 보기
+                        </Button>} severity="info">역할 통계 데이터가 존재하지 않습니다. 프로젝트에 참여하여 새로운 역할을 받아보세요!</Alert></Grid>
+                }
+
                 {showEvalStatistics ?
                     <Grid item sm={12} lg={6} minHeight={160}><ProfileRadar data={evalStatistics} user_id={user?.user_id} /></Grid>
                     : <Grid xs={12}><Alert action={
@@ -205,62 +88,8 @@ const ProfilePortpolio = () => {
                             내 워크스페이스 가기
                         </Button>} severity="info">상호평가 통계 데이터가 존재하지 않습니다. 프로젝트 워크스페이스로 이동해 종료 프로젝트에서 상호평가를 진행해보세요!</Alert></Grid>
                 }
+
             </Grid>
-
-            {/* <div className="my-3">
-                    <br />
-                    
-                    <h4>현재 소속 그룹의 역할</h4>
-                    <div className="my-3 row row-cols-1 row-cols-md-2 row-cols-xl-3 g-3 align-items-stretch ">
-                                {
-                        groups.length > 0
-                            ?
-                            <>
-                                {
-                                    groups.map((group) => {
-                                        if (group.end_project === false) {
-                                            return <RoleCard
-                                                key={group._id}
-                                                group={group}
-                                                setSelectedGroup={setSelectedGroup}
-                                            />
-                                        }
-                                    })
-                                }
-                            </>
-                            :
-                            <div>프로젝트가 없습니다.</div>
-                    }
-                    </div>
-                    </div>
-
-                    <div className="my-3">
-                    <br />
-                <h4>과거 소속 그룹의 역할
-                    </h4> 
-                <div className="my-3 row row-cols-1 row-cols-md-2 row-cols-xl-3 g-3 align-items-stretch ">
-                    {
-                        groups.length > 0
-                            ?
-                            <>
-                                {
-                                    groups.map((group) => {
-                                        if (group.end_project === true) {
-                                            return <RoleCard
-                                                key={group._id}
-                                                group={group}
-                                                setSelectedGroup={setSelectedGroup}
-                                        />;
-                                        }
-                                    })
-                                }
-                            </>
-                            :
-                            <div>프로젝트가 없습니다.</div>
-                    }
-                </div>
-            </div> */}
-
         </>
     );
 };
--- src/container/sign/profile/view/ProfileRadar.js
+++ src/container/sign/profile/view/ProfileRadar.js
@@ -1,30 +1,30 @@
 import { Box, Paper, Typography } from '@mui/material';
 import { ResponsiveRadar } from '@nivo/radar'
 
-const ProfileRadar = ({data, user_id}) => {
+const ProfileRadar = ({ data, user_id }) => {
     return (
         <Paper>
             <Typography variant='h5' align='center' pt={2}>상호평가 점수</Typography>
             <div style={{ width: '100%', height: '500px', margin: '0 auto' }}>
-            <ResponsiveRadar
-                data={data}
-                keys={[user_id]}
-                maxValue={5}
-                indexBy="label"
-                margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-                borderColor={{ from: 'color' }}
-                gridLabelOffset={12}
-                dotSize={10}
-                dotColor={{ theme: 'background' }}
-                dotBorderWidth={2}
-                colors={{ scheme: 'accent' }}
-                blendMode="multiply"
-                motionConfig="wobbly"
-                gridShape="linear"
-                enableDotLabel={true}
-                dotLabel="value"
-                dotLabelYOffset={24}
-            />
+                <ResponsiveRadar
+                    data={data}
+                    keys={[user_id]}
+                    maxValue={5}
+                    indexBy="label"
+                    margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+                    borderColor={{ from: 'color' }}
+                    gridLabelOffset={12}
+                    dotSize={10}
+                    dotColor={{ theme: 'background' }}
+                    dotBorderWidth={2}
+                    colors={{ scheme: 'accent' }}
+                    blendMode="multiply"
+                    motionConfig="wobbly"
+                    gridShape="linear"
+                    enableDotLabel={true}
+                    dotLabel="value"
+                    dotLabelYOffset={24}
+                />
             </div>
         </Paper>
     )
