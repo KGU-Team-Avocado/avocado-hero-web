59c191a6c4cfe9bc2c68897d3f07f4e5f6a2b9f6
soyoung125 <sypark041754@gmail.com>
2022-11-17 20:47:59 +0900

    feat: 백엔드에서ㅓ 불러온 데이터러 그래프 그리기

--- src/container/sign/profile/view/ProfileRadar.js
+++ src/container/sign/profile/view/ProfileRadar.js
@@ -1,41 +1,14 @@
 import { ResponsiveRadar } from '@nivo/radar'
 
-const ProfileRadar = () => {
-    const data = [
-        {
-            "taste": "질적 공헌도",
-            "user_id": 2.7
-            
-        },
-        {
-            "taste": "양적 공헌도",
-            "user_id": 3.9
-            
-        },
-        {
-            "taste": "참여 태도",
-            "user_id": 5.0
-            
-        },
-        {
-            "taste": "책임감",
-            "user_id": 1.3
-
-        },
-        {
-            "taste": "적합성",
-            "user_id": 4.5
-        }
-    ]
-
+const ProfileRadar = ({data, user_id}) => {
     return (
         <div style={{ width: '100%', height: '500px', margin: '0 auto' }}>
             <div>상호 평가 통계(데이터 구현 예정)</div>
             <ResponsiveRadar
                 data={data}
-                keys={['user_id']}
+                keys={[user_id]}
                 maxValue={5}
-                indexBy="taste"
+                indexBy="label"
                 margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
                 borderColor={{ from: 'color' }}
                 gridLabelOffset={12}
