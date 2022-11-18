ea93a7492c810ad777486d76812ad25ed2665fad
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-18 17:01:33 +0900

    feat: readme modal에서 그룹정보가 연동되는 기능 추가

--- src/container/group/workspace/readme/ReadmeModal.js
+++ src/container/group/workspace/readme/ReadmeModal.js
@@ -1,9 +1,13 @@
 import React from "react"
-import { Box, DialogContent, DialogTitle, Divider, IconButton, Stack, TextField, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Avatar, Box, DialogContent, DialogTitle, Divider, IconButton, List, ListItem, ListItemAvatar, ListItemText, Stack, TextField, Typography } from "@mui/material";
 import ClearIcon from '@mui/icons-material/Clear';
 import MKButton from "component/common/mui-components/MKButton";
 import { useNavigate } from "react-router-dom";
 import Readme from "./Readme";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 
 const ReadmeModal = ({ group, setOpen }) => {
 
@@ -34,36 +38,87 @@ const ReadmeModal = ({ group, setOpen }) => {
                 <DialogTitle id="scroll-dialog-title">
                     <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Typography variant="h4" >
-                            {group?.project_name}의 Read Me
+                            {group?.project_name}
                         </Typography>
                         <IconButton size="large" onClick={() => setOpen(false)}><ClearIcon fontSize="inherit" /></IconButton >
                     </Box>
                 </DialogTitle>
                 <DialogContent dividers={true}>
                     <Stack spacing={1}>
-                        <Typography variant="h5">팀명</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">프로젝트명</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">요약</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">태그</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">팀장</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">인원수</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">상태</Typography>
-                        <Typography>ㅇㅇ</Typography>
-                        <Divider />
-                        <Typography variant="h5">프로젝트 기간</Typography>
-                        <Typography>ㅇㅇ</Typography>
+                        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <ImageIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="팀명" secondary={group?.group_name} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <WorkIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="프로젝트명" secondary={group?.project_name} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="요약" secondary={group?.short_description} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="태그" secondary={group?.tech_stack} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="팀장" secondary={group?.manager} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="요약" secondary={group?.short_description} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="인원수" secondary={group?.members.length} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="상태" secondary={`${group?.close_application}${group?.end_project}`} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="프로젝트 기간" secondary={`${group?.start_date}~${group?.end_date}`} />
+                            </ListItem>
+                        </List>
                         <Divider />
                         <Readme group_id={group?._id} />
                     </Stack>
