d407501f3e43300b0659911318821b4baa6ba328
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-23 00:02:16 +0900

    feat: 프로필 수정 v2 뼈대 업데이트

--- src/container/sign/profile/edit_v2/ProfileEditV2.js
+++ src/container/sign/profile/edit_v2/ProfileEditV2.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Stack, TextField, Typography } from "@mui/material";
 import ResponsiveCard from "component/common/ResponsiveCard";
 import { useEffect, useState } from "react";
 import Avatar from "../avatar/Avatar";
@@ -11,6 +11,14 @@ const ProfileEditV2 = () => {
     const user = useSelector(selectUser);
     const [profile, setProfile] = useState(null);
 
+    const handleInput = (state) => {
+        console.log(state)
+        setProfile({
+            ...profile,
+            [state.target.id]: state.target.value
+        })
+    }
+
     useEffect(() => {
         getUserProfile();
     }, [user]);
@@ -21,11 +29,149 @@ const ProfileEditV2 = () => {
 
     return (
         <>
+            {JSON.stringify(profile)}
             <Typography variant="h3">프로필 수정</Typography>
-            <ResponsiveCard>
-                <Avatar edit={true} user_id={profile?.user_id} imgURL={profile?.imgURL}/>
-                {JSON.stringify(profile)}
-            </ResponsiveCard>
+            <Box marginBottom={2}>
+                <ResponsiveCard>
+                    <>
+                        <Avatar edit={true} user_id={profile?.user_id} imgURL={profile?.imgURL} />
+                        <Box marginBottom={2}>
+                            <TextField
+                                fullWidth
+                                label="닉네임"
+                                variant="outlined"
+                                value={profile?.nickname}
+                                id="nickname"
+                                onChange={handleInput}
+                                focused
+                            />
+                        </Box>
+                        <Box marginBottom={2}>
+                            <TextField
+                                fullWidth
+                                label="이름"
+                                variant="outlined"
+                                value={profile?.name}
+                                id="name"
+                                onChange={handleInput}
+                                focused
+                            />
+                        </Box>
+                        <Box marginBottom={2}>
+                            <TextField
+                                fullWidth
+                                label="이메일"
+                                variant="outlined"
+                                value={profile?.email}
+                                id="email"
+                                onChange={handleInput}
+                                focused
+                            />
+                        </Box>
+                        <Box marginBottom={2}>
+                            <TextField
+                                fullWidth
+                                label="전화번호"
+                                variant="outlined"
+                                value={profile?.phoneNumber}
+                                id="phoneNumber"
+                                onChange={handleInput}
+                                focused
+                            />
+                        </Box>
+                    </>
+                </ResponsiveCard>
+            </Box>
+            <Box marginBottom={2}>
+                <ResponsiveCard>
+                    <Box>
+                        <TextField
+                            fullWidth
+                            label="한줄소개"
+                            variant="outlined"
+                            value={profile?.introduceOne}
+                            id="introduceOne"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                </ResponsiveCard>
+            </Box>
+            <Box marginBottom={2}>
+                <ResponsiveCard>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="소속"
+                            variant="outlined"
+                            value={profile?.belongs}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="분야"
+                            variant="outlined"
+                            value={profile?.belongs}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="링크"
+                            variant="outlined"
+                            value={profile?.links}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                </ResponsiveCard>
+            </Box>
+            <Box marginBottom={2}>
+                <ResponsiveCard>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="키워드"
+                            variant="outlined"
+                            value={profile?.keywords}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="성향"
+                            variant="outlined"
+                            value={profile?.personalities}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField
+                            fullWidth
+                            label="소개글"
+                            variant="outlined"
+                            value={profile?.introduce}
+                            id="belongs"
+                            onChange={handleInput}
+                            focused
+                        />
+                    </Box>
+                </ResponsiveCard>
+            </Box>
+
         </>
     )
 }
